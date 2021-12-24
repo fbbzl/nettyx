f0a2912f87aa69f7e7905db88fd916510e5d3894
fbbzl <290361638@qq.com>
2021-12-24 20:57:11 +0800

    feature(project): upgrade
    optimize and upgrade

--- src/main/java/org/fz/nettyx/server/Server.java
+++ src/main/java/org/fz/nettyx/server/Server.java
@@ -1,5 +1,9 @@
 package org.fz.nettyx.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -11,6 +15,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public abstract class Server {
 
+    private EventLoopGroup
+        parentGroup = new NioEventLoopGroup(),
+        childGroup = new NioEventLoopGroup();
+    private ServerBootstrap serverBootstrap = new ServerBootstrap().group(parentGroup, childGroup).channel(NioServerSocketChannel.class);
 
 
 }
