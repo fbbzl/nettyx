3fa0862984c5825c869c5bcddb71d4763e034fb3
fbbzl <290361638@qq.com>
2022-01-03 14:23:32 +0800

    feature(server): add server
    add server

--- src/main/java/org/fz/nettyx/server/Server.java
+++ src/main/java/org/fz/nettyx/server/Server.java
@@ -4,20 +4,56 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import java.net.SocketAddress;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * @author fengbinbin
- * @version 1.0
- * @since 2021/11/25 15:53
- */
-
 @Slf4j
+@SuppressWarnings("unchecked")
 public abstract class Server {
 
-    private EventLoopGroup
-        parentGroup = new NioEventLoopGroup(1),
-        childGroup = new NioEventLoopGroup();
-    private ServerBootstrap serverBootstrap = new ServerBootstrap().group(parentGroup, childGroup).channel(NioServerSocketChannel.class);
+    private final EventLoopGroup
+        parentEventLoopGroup = new NioEventLoopGroup(),
+        childEventLoopGroup  = new NioEventLoopGroup();
+
+    private final ServerBootstrap serverBootstrap = new ServerBootstrap().group(parentEventLoopGroup, childEventLoopGroup)
+        .channel(NioServerSocketChannel.class);
+
+    public EventLoopGroup parentEventLoopGroup() {
+        return this.parentEventLoopGroup;
+    }
+
+    public EventLoopGroup childEventLoopGroup() {
+        return this.childEventLoopGroup;
+    }
+
+    protected ServerBootstrap newServerBootstrap() {
+        return serverBootstrap.clone();
+    }
+
+    public abstract void bind(SocketAddress socketAddress) throws Exception;
+
+    protected void shutdownGracefully() {
+        childEventLoopGroup.shutdownGracefully();
+        parentEventLoopGroup.shutdownGracefully();
+    }
+
+    public <T> ScheduledFuture<T> schedule(Runnable command, long delay, TimeUnit unit) {
+        return (ScheduledFuture<T>) parentEventLoopGroup().schedule(command, delay, unit);
+    }
+
+    public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
+        return parentEventLoopGroup().schedule(callable, delay, unit);
+    }
+
+    public <T> ScheduledFuture<T> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
+        return (ScheduledFuture<T>) parentEventLoopGroup().scheduleAtFixedRate(command, initialDelay, period, unit);
+    }
+
+    public <T> ScheduledFuture<T> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
+        return (ScheduledFuture<T>) parentEventLoopGroup().scheduleWithFixedDelay(command, initialDelay, delay, unit);
+    }
 
 }
