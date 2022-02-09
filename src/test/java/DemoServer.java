38055ab01cc4ce37fa54c9e7477f521ba6ced91c
503280366 <503280366@ge.com>
2022-02-09 12:55:15 +0800

    refactor(codec): upgrade
    upgrade

--- src/test/java/DemoServer.java
+++ src/test/java/DemoServer.java
@@ -1,11 +1,14 @@
-import static java.nio.charset.StandardCharsets.UTF_8;
-
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
-import org.fz.nettyx.codec.StringMessageCodec;
+import java.util.concurrent.TimeUnit;
 import org.fz.nettyx.server.Server;
 
 /**
@@ -23,7 +26,31 @@ public class DemoServer extends Server {
         return new ChannelInitializer<NioServerSocketChannel>() {
             @Override
             protected void initChannel(NioServerSocketChannel ch) {
-                ch.pipeline().addLast(new StringMessageCodec(UTF_8));
+                ch.pipeline()
+                    .addLast(new IdleStateHandler(5, 0, 0, TimeUnit.SECONDS))
+                    .addLast(new ChannelInboundHandlerAdapter() {
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            System.err.println(msg);
+                            super.channelRead(ctx, msg);
+                        }
+
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            System.err.println("jihuo");
+                            super.channelActive(ctx);
+                        }
+                        @Override
+                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+                            System.out.println("客户端循环心跳监测发送: ");
+                            if (evt instanceof IdleStateEvent) {
+                                IdleStateEvent event = (IdleStateEvent) evt;
+                                if (event.state() == IdleState.WRITER_IDLE) {
+                                    ctx.writeAndFlush("biubiu");
+                                }
+                            }
+                        }
+                    });
             }
         };
     }
@@ -31,7 +58,7 @@ public class DemoServer extends Server {
     public static void main(String[] args) throws Exception {
         DemoServer demoServer = new DemoServer();
         try {
-            demoServer.bind(new InetSocketAddress(8888)).channel().closeFuture().sync();
+            demoServer.bind(new InetSocketAddress(5655)).channel().closeFuture().sync();
         } finally {
             demoServer.shutdownGracefully();
         }
