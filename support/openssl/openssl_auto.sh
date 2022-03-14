78c36d0a703bf1e9b623275eb91a0b3170eb98de
fbbzl <290361638@qq.com>
2022-03-14 22:02:12 +0800

    refactor(project): UPGRADE
    UPGRADE

--- support/openssl/openssl_auto.sh
+++ support/openssl/openssl_auto.sh
@@ -14,17 +14,27 @@ mkdir -p ${os_openssl_ca}
 cp -f ${work_dir}/cnf/openssl.cnf ${os_openssl_ca}/
 
 #init openssl env-dir
+echo '####################################'
+echo '# Initializing OpenSSL Directories #'
+echo '####################################'
 sh ${work_dir}/openssl_init_dir.sh
 
 #init ca
+echo '########################'
+echo '# Initializing CA ROOT #'
+echo '########################'
 sh ${work_dir}/ca/openssl_init_ca_root.sh
 
-#init server
+echo '#######################'
+echo '# Initializing Server #'
+echo '#######################'
 sh ${work_dir}/server/openssl_server_init.sh
 sh ${work_dir}/server/openssl_server_do_sign.sh
 sh ${work_dir}/server/openssl_server_pkcs8_key.sh
 
-#init client
+echo '#######################'
+echo '# Initializing Client #'
+echo '#######################'
 sh ${work_dir}/client/openssl_client_init.sh
 sh ${work_dir}/client/openssl_client_do_sign.sh
 sh ${work_dir}/client/openssl_client_pkcs8_key.sh
