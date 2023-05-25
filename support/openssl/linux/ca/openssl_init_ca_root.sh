f08d19b561719429c72d60cd9084ff548b7143fa
fengbinbin <290361638@qq.com>
2023-05-25 22:54:41 +0800

    build(upgrade): upgrade script

--- support/openssl/linux/ca/openssl_init_ca_root.sh
+++ support/openssl/linux/ca/openssl_init_ca_root.sh
@@ -6,16 +6,10 @@
 #                                                                                #
 ##################################################################################
 
-os_openssl_ca=/etc/pki/CA
-os_openssl_ca_cnf=${os_openssl_ca}/openssl.cnf
-
-app_name=youappname
-app=/usr/local/yourapplocation/${app_name}
-app_openssl_dir=${app}/openssl
-
-ca_root_key=${app_openssl_dir}/root/ca_root.key
-ca_root_crt=${app_openssl_dir}/root/ca_root.crt
+ca_root_key=${ca_root_dir}/ca_root.key
+ca_root_crt=${ca_root_dir}/ca_root.crt
 #100 years
 valid_days=36500
 
-openssl req -new -x509 -keyout ${ca_root_key} -out ${ca_root_crt} -config ${os_openssl_ca_cnf} -days ${valid_days}
+# shellcheck disable=SC2154
+openssl req -new -x509 -keyout "${ca_root_key}" -out "${ca_root_crt}" -config "${os_openssl_ca_cnf}" -days ${valid_days}
--- support/openssl/linux/server/openssl_server_init.sh
+++ support/openssl/linux/server/openssl_server_init.sh
@@ -6,21 +6,15 @@
 #                                                                                #
 ##################################################################################
 
-os_openssl_ca=/etc/pki/CA
-os_openssl_cnf=${os_openssl_ca}/openssl.cnf
-
-app_name=youappname
-app=/usr/local/yourapplocation/${app_name}
-app_openssl_dir=${app}/openssl
-
 server_key=${app_openssl_dir}/server/${app_name}_server.key
 server_csr=${app_openssl_dir}/server/${app_name}_server.csr
 
-key_pass=Aqqaazz123!
+key_pass=Aqqaazz123!@@
 key_length=2048
 #100 years
 valid_days=36500
 
-openssl genrsa -des3 -passout pass:${key_pass} -out ${server_key} ${key_length}
+openssl genrsa -des3 -passout pass:${key_pass} -out "${server_key}" ${key_length}
 
-openssl req -new -key ${server_key} -out ${server_csr} -config ${os_openssl_cnf} -days ${valid_days}
+# shellcheck disable=SC2154
+openssl req -new -key "${server_key}" -out "${server_csr}" -config "${os_openssl_cnf}" -days ${valid_days}
