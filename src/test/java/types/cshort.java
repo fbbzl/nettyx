ae2634164632af1c9853b0e3077ee03885faa47b
fengbinbin <290361638@qq.com>
2023-12-13 22:12:30 +0800

    feature(upgrade): upgrade

---
+++ src/test/java/types/cshort.java
@@ -0,0 +1,49 @@
+package types;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import org.fz.nettyx.serializer.typed.Basic;
+
+import java.nio.ByteOrder;
+
+/**
+ * @author fengbinbin
+ * @since 2021-10-19 18:00
+ **/
+public class cshort extends Basic<Short> {
+
+    public cshort(Short value) {
+        super(value);
+    }
+
+    public cshort(int value) {
+        super((short) value);
+    }
+
+    @Override
+    public boolean hasSinged() {
+        return true;
+    }
+
+    @Override
+    public ByteOrder order() {
+        return ByteOrder.LITTLE_ENDIAN;
+    }
+
+    @Override
+    protected ByteBuf toByteBuf(Short value) {
+        return Unpooled.buffer(size()).writeShortLE(value);
+    }
+
+    @Override
+    protected Short toValue(ByteBuf byteBuf) {
+        return byteBuf.readShortLE();
+    }
+
+    @Override
+    public int size() {
+        return 2;
+    }
+
+
+}
