fa1710f433a744c88355b0e8f129ee931e8b99a2
fbb <290361638@qq.com>
2024-01-08 09:21:08 +0800

    feat(add cache): use hutool use hutool

---
+++ src/main/java/org/fz/nettyx/serializer/struct/basic/cpp/signed/Cppshort.java
@@ -0,0 +1,31 @@
+package org.fz.nettyx.serializer.struct.basic.cpp.signed;
+
+import io.netty.buffer.ByteBuf;
+import org.fz.nettyx.serializer.struct.basic.c.signed.Cshort;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2023/12/27 13:20
+ */
+public class Cppshort extends Cshort {
+
+    /**
+     * The constant MIN_VALUE.
+     */
+    public static final Cppshort MIN_VALUE = new Cppshort(Integer.valueOf(Short.MIN_VALUE));
+
+    /**
+     * The constant MAX_VALUE.
+     */
+    public static final Cppshort MAX_VALUE = new Cppshort(Integer.valueOf(Short.MAX_VALUE));
+
+    public Cppshort(Integer value) {
+        super(value);
+    }
+
+    public Cppshort(ByteBuf buf) {
+        super(buf);
+    }
+
+}
