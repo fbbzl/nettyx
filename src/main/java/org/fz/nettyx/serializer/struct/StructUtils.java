1eac16ad834feb20f7b14d1998d855bc7b6c6a96
fbb <290361638@qq.com>
2024-01-08 09:21:08 +0800

    feat(annotation): hutool
    use hutool to instead some methods

--- src/main/java/org/fz/nettyx/serializer/struct/StructUtils.java
+++ src/main/java/org/fz/nettyx/serializer/struct/StructUtils.java
@@ -6,7 +6,7 @@ import static org.fz.nettyx.serializer.struct.PropertyHandler.getTargetAnnotatio
 import static org.fz.nettyx.serializer.struct.StructSerializer.isBasic;
 import static org.fz.nettyx.serializer.struct.StructSerializer.isStruct;
 import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.ANNOTATION_HANDLER_MAPPING_CACHE;
-import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.BASIC_CONSTRUCTOR_CACHE;
+import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.BASIC_BUF_CONSTRUCTOR_CACHE;
 import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.FIELD_READER_CACHE;
 import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.FIELD_WRITER_CACHE;
 import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.STRUCT_CONSTRUCTOR_CACHE;
@@ -222,7 +222,7 @@ public class StructUtils {
      * @return the basic constructor
      */
     <B extends Basic<?>> Constructor<B> getBasicConstructor(Class<B> basicClass) {
-        return (Constructor<B>) BASIC_CONSTRUCTOR_CACHE.computeIfAbsent(basicClass, c -> {
+        return (Constructor<B>) BASIC_BUF_CONSTRUCTOR_CACHE.computeIfAbsent(basicClass, c -> {
             try {
                 return c.getConstructor(ByteBuf.class);
             } catch (NoSuchMethodException exception) {
@@ -465,7 +465,7 @@ public class StructUtils {
          * The constant BASIC_CONSTRUCTOR_CACHE.
          */
         /* constructor cache */
-        static final Map<Class<? extends Basic<?>>, Constructor<? extends Basic<?>>> BASIC_CONSTRUCTOR_CACHE = new ConcurrentHashMap<>(
+        static final Map<Class<? extends Basic<?>>, Constructor<? extends Basic<?>>> BASIC_BUF_CONSTRUCTOR_CACHE = new ConcurrentHashMap<>(
             512);
         /**
          * The Struct constructor cache.
@@ -486,6 +486,7 @@ public class StructUtils {
         static {
             try {
                 scanHandlers();
+                scanBasics();
                 scanStructs();
             } catch (Exception e) {
                 throw new NotInitedException("init nettyx serializer cache failed please check", e);
@@ -508,6 +509,15 @@ public class StructUtils {
                         StructUtils.getHandlerConstructor(
                             ((Class<? extends PropertyHandler<? extends Annotation>>) handlerClass)));
                 }
+            }
+        }
+
+        private static synchronized void scanBasics() throws NoSuchMethodException {
+            Set<Class<?>> basicClasses = ClassScanner.scanPackageBySuper(ALL_PACKAGE, Basic.class);
+            for (Class<?> basicClass : basicClasses) {
+                if (Modifier.isAbstract(basicClass.getModifiers())) continue;
+                Constructor<? extends Basic<?>> basicConstructor = (Constructor<? extends Basic<?>>) basicClass.getConstructor(ByteBuf.class);
+                BASIC_BUF_CONSTRUCTOR_CACHE.putIfAbsent((Class<? extends Basic<?>>) basicClass, basicConstructor);
             }
         }
 
--- src/main/java/org/fz/nettyx/serializer/struct/annotation/collection/ToArrayList.java
+++ src/main/java/org/fz/nettyx/serializer/struct/annotation/collection/ToArrayList.java
@@ -6,12 +6,12 @@ import static org.fz.nettyx.serializer.struct.StructUtils.nullDefault;
 import static org.fz.nettyx.serializer.struct.annotation.collection.ToArray.ToArrayHandler.readArray;
 import static org.fz.nettyx.serializer.struct.annotation.collection.ToArray.ToArrayHandler.writeArray;
 
+import cn.hutool.core.collection.ListUtil;
 import io.netty.buffer.ByteBuf;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.fz.nettyx.serializer.struct.PropertyHandler;
 import org.fz.nettyx.serializer.struct.StructSerializer;
@@ -52,7 +52,7 @@ public @interface ToArrayList {
             StructUtils.checkAssignable(field, List.class);
             Class<?> elementType = toArrayList.elementType();
 
-            return new ArrayList<>(Arrays.asList(readArray(elementType, toArrayList.size(), serializer.getByteBuf())));
+            return ListUtil.toList(readArray(elementType, toArrayList.size(), serializer.getByteBuf()));
         }
 
         @Override
