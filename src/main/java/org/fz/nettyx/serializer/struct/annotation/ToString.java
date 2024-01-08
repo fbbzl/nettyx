c112fd6554ca37e1937ff3e5ea8a7260d19419f0
fbb <290361638@qq.com>
2024-01-08 16:24:24 +0800

    feat(add cache): move annotation  move annotation into upper-level package

--- src/main/java/org/fz/nettyx/serializer/struct/annotation/ToString.java
+++ src/main/java/org/fz/nettyx/serializer/struct/annotation/ToString.java
@@ -1,4 +1,4 @@
-package org.fz.nettyx.serializer.struct.annotation.collection;
+package org.fz.nettyx.serializer.struct.annotation;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
