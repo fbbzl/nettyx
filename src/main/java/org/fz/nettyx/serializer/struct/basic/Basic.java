b8cca9b1f5b610e3f4cd1017a0ee1ce7178dae0d
fengbinbin <290361638@qq.com>
2024-01-10 20:06:44 +0800

    feat(cache): array handler
    handler array handler

--- src/main/java/org/fz/nettyx/serializer/struct/basic/Basic.java
+++ src/main/java/org/fz/nettyx/serializer/struct/basic/Basic.java
@@ -85,6 +85,7 @@ public abstract class Basic<V> {
         this.size = size;
         this.value = value;
         this.bytes = new byte[this.size];
+
         ByteBuf buf = this.toByteBuf(this.value, this.size);
         this.fill(buf, this.size);
         buf.readBytes(this.bytes);
