abd0ed8ef165bcbfa2e91ad14af7ffb7bec86c07
fengbinbin <290361638@qq.com>
2024-01-12 20:47:44 +0800

    feat(cache): array handler
    handler array handler

--- src/main/java/org/fz/nettyx/serializer/struct/basic/cpp/CppBasic.java
+++ src/main/java/org/fz/nettyx/serializer/struct/basic/cpp/CppBasic.java
@@ -11,13 +11,13 @@ import org.fz.nettyx.serializer.struct.basic.Basic;
  * @version 1.0
  * @since 2023 /12/15 16:04
  */
-public abstract class CppBasic<V extends Comparable<V>> extends Basic<V> {
+public abstract class CppBasic<V extends Comparable<V>> extends Basic<V> implements Comparable<CppBasic<V>> {
 
     private static final boolean CPP_DEFAULT_SINGED = true;
     private static final ByteOrder CPP_DEFAULT_ENDIAN = ByteOrder.LITTLE_ENDIAN;
 
 
-    protected CppBasic(V value, int size) {
+    protected CppBasic(Object value, int size) {
         super(value, size);
     }
 
@@ -34,6 +34,11 @@ public abstract class CppBasic<V extends Comparable<V>> extends Basic<V> {
     @Override
     public ByteOrder order() {
         return CPP_DEFAULT_ENDIAN;
+    }
+
+    @Override
+    public int compareTo(CppBasic<V> anotherCBasic) {
+        return this.getValue().compareTo(anotherCBasic.getValue());
     }
 
 }
--- src/main/java/org/fz/nettyx/serializer/struct/basic/cpp/signed/Cpp16tchar.java
+++ src/main/java/org/fz/nettyx/serializer/struct/basic/cpp/signed/Cpp16tchar.java
@@ -13,7 +13,7 @@ import org.fz.nettyx.serializer.struct.basic.cpp.CppBasic;
  */
 public class Cpp16tchar extends CppBasic<Character> {
 
-    public Cpp16tchar(Character value) {
+    public Cpp16tchar(Object value) {
         super(value, 2);
     }
 
@@ -22,8 +22,8 @@ public class Cpp16tchar extends CppBasic<Character> {
     }
 
     @Override
-    protected ByteBuf toByteBuf(Object value, int size) {
-        return Unpooled.buffer(getSize()).writeChar((Character) value);
+    protected ByteBuf toByteBuf(Character value, int size) {
+        return Unpooled.buffer(getSize()).writeChar(value);
     }
 
     @Override
