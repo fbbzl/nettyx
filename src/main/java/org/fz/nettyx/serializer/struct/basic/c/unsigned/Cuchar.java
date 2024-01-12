9c8f9fd9322b665567c5db52d3d02d469338b755
fengbinbin <290361638@qq.com>
2024-01-12 22:35:26 +0800

    feat(cache): array handler
    handler array handler

--- src/main/java/org/fz/nettyx/serializer/struct/basic/c/unsigned/Cuchar.java
+++ src/main/java/org/fz/nettyx/serializer/struct/basic/c/unsigned/Cuchar.java
@@ -30,8 +30,8 @@ public class Cuchar extends CBasic<Short> {
      *
      * @param value the length
      */
-    public Cuchar(Integer value) {
-        super(value.shortValue(), 1);
+    public Cuchar(Object value) {
+        super(((Integer) value).shortValue(), 1);
     }
 
     /**
