546877a1ded5767973ab7fbe9e4da61107674c66
fbb <290361638@qq.com>
2024-01-16 11:18:34 +0800

    feat(add cache): use hutool use hutool

--- src/main/java/org/fz/nettyx/serializer/struct/annotation/collection/ToArrayList.java
+++ src/main/java/org/fz/nettyx/serializer/struct/annotation/collection/ToArrayList.java
@@ -17,7 +17,6 @@ import java.util.List;
 import org.fz.nettyx.exception.ParameterizedTypeException;
 import org.fz.nettyx.serializer.struct.PropertyHandler;
 import org.fz.nettyx.serializer.struct.StructSerializer;
-import org.fz.nettyx.serializer.struct.StructUtils;
 import org.fz.nettyx.util.Throws;
 
 /**
@@ -54,7 +53,7 @@ public @interface ToArrayList {
         @Override
         public Object doRead(StructSerializer serializer, Field field, ToArrayList toArrayList) {
             Class<?> elementType =
-                (elementType = StructUtils.getFieldParameterizedType(serializer.getStructParameterizedType(), field)) == Object.class ? toArrayList.elementType()
+                (elementType = serializer.getFieldActualType(field)) == Object.class ? toArrayList.elementType()
                     : elementType;
 
             Throws.ifTrue(elementType == Object.class, new ParameterizedTypeException(field));
@@ -66,7 +65,7 @@ public @interface ToArrayList {
         public void doWrite(StructSerializer serializer, Field field, Object value, ToArrayList toArrayList,
             ByteBuf writing) {
             Class<?> elementType =
-                (elementType = StructUtils.getFieldParameterizedType(serializer.getStructParameterizedType(), field)) == Object.class ? toArrayList.elementType()
+                (elementType = serializer.getFieldActualType(field)) == Object.class ? toArrayList.elementType()
                     : elementType;
 
             Throws.ifTrue(elementType == Object.class, new ParameterizedTypeException(field));
