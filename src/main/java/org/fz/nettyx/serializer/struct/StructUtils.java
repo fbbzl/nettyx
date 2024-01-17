3e8a9a5fcd12a4145820cc31bb1e34390acf4ad5
fengbinbin <290361638@qq.com>
2024-01-17 22:10:24 +0800

    feat(cache): array handler
    handler array handler

--- src/main/java/org/fz/nettyx/serializer/struct/StructUtils.java
+++ src/main/java/org/fz/nettyx/serializer/struct/StructUtils.java
@@ -3,11 +3,11 @@ package org.fz.nettyx.serializer.struct;
 import static org.fz.nettyx.serializer.struct.PropertyHandler.getTargetAnnotationType;
 import static org.fz.nettyx.serializer.struct.PropertyHandler.isReadHandler;
 import static org.fz.nettyx.serializer.struct.PropertyHandler.isWriteHandler;
+import static org.fz.nettyx.serializer.struct.StructSerializer.isStruct;
 import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.ANNOTATION_HANDLER_MAPPING_CACHE;
 import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.BASIC_BYTES_SIZE_CACHE;
 import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.FIELD_READER_CACHE;
 import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.FIELD_WRITER_CACHE;
-import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.TRANSIENT_FIELD_CACHE;
 
 import cn.hutool.core.annotation.AnnotationUtil;
 import cn.hutool.core.bean.BeanUtil;
@@ -37,7 +37,6 @@ import java.util.function.Predicate;
 import lombok.experimental.UtilityClass;
 import org.fz.nettyx.exception.SerializeException;
 import org.fz.nettyx.exception.TooLessBytesException;
-import org.fz.nettyx.serializer.struct.annotation.Ignore;
 import org.fz.nettyx.serializer.struct.annotation.Struct;
 import org.fz.nettyx.serializer.struct.basic.Basic;
 import org.fz.nettyx.util.Try;
@@ -53,68 +52,6 @@ import org.fz.nettyx.util.Try;
 @SuppressWarnings("unchecked")
 @UtilityClass
 public class StructUtils {
-
-    public boolean isTransient(Field field) {
-        return TRANSIENT_FIELD_CACHE.contains(field);
-    }
-
-    public static <T> boolean isNotBasic(T object) {
-        return isNotBasic(object.getClass());
-    }
-
-    public static boolean isNotBasic(Field field) {
-        return isNotBasic(field.getType());
-    }
-
-    public static boolean isNotBasic(Class<?> clazz) {
-        return !isBasic(clazz);
-    }
-
-    public static <T> boolean isBasic(T object) {
-        return isBasic(object.getClass());
-    }
-
-    public static boolean isBasic(Field field) {
-        return isBasic(field.getType());
-    }
-
-    public static boolean isBasic(Class<?> clazz) {
-        return Basic.class.isAssignableFrom(clazz) && Basic.class != clazz;
-    }
-
-    public static boolean isNotStruct(Field field) {
-        return isNotStruct(field.getType());
-    }
-
-    public static <T> boolean isNotStruct(T object) {
-        return isNotStruct(object.getClass());
-    }
-
-    public static boolean isNotStruct(Class<?> clazz) {
-        return !isStruct(clazz);
-    }
-
-    public static boolean isStruct(Field field) {
-        return isStruct(field.getType());
-    }
-
-    public static <T> boolean isStruct(T object) {
-        return isStruct(object.getClass());
-    }
-
-    public static boolean isStruct(Class<?> clazz) {
-        return AnnotationUtil.hasAnnotation(clazz, Struct.class);
-    }
-
-    /**
-     * Is ignore boolean.
-     *
-     * @param field the field
-     * @return the boolean
-     */
-    public static boolean isIgnore(Field field) {
-        return AnnotationUtil.hasAnnotation(field, Ignore.class) || StructUtils.isTransient(field);
-    }
 
     /**
      * Is read handleable boolean.
