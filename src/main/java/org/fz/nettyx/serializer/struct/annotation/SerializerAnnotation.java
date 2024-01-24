2a958bac82c11113dc6bfe04908767255969d5e3
fengbinbin <290361638@qq.com>
2024-01-24 15:14:57 +0800

    feat(update): c type
    add c type

---
+++ src/main/java/org/fz/nettyx/serializer/struct/annotation/SerializerAnnotation.java
@@ -0,0 +1,22 @@
+package org.fz.nettyx.serializer.struct.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * top serializer annotation, every property serializer annotation must add with this annotation
+ *
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/1/24 15:11
+ */
+
+@Documented
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface SerializerAnnotation {
+}
