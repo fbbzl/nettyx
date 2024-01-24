6fec0162e92bbcfaab3c10bb9c9e2d0c97eafb02
fengbinbin <290361638@qq.com>
2024-01-24 16:02:12 +0800

    feat(update): c type
    add c type

---
+++ src/main/java/org/fz/nettyx/serializer/struct/StructSerializerManagement.java
@@ -0,0 +1,18 @@
+package org.fz.nettyx.serializer.struct;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/1/24 15:54
+ */
+
+public class StructSerializerManagement {
+
+    private String[] scanStructPackages;
+
+    public StructSerializerManagement(String... scanStructPackages) {
+        this.scanStructPackages = scanStructPackages;
+
+
+    }
+}
