aefab4908aed5da6cad13631bbea5351a97ec08d
fengbinbin <290361638@qq.com>
2024-01-24 16:09:03 +0800

    feat(update): c type
    add c type

--- src/main/java/org/fz/nettyx/serializer/struct/StructSerializerManagement.java
+++ src/main/java/org/fz/nettyx/serializer/struct/StructSerializerManagement.java
@@ -1,17 +1,25 @@
 package org.fz.nettyx.serializer.struct;
 
+import lombok.Getter;
+
+import static cn.hutool.core.util.ArrayUtil.distinct;
+
 /**
  * @author fengbinbin
  * @version 1.0
  * @since 2024/1/24 15:54
  */
 
+@Getter
 public class StructSerializerManagement {
 
-    private String[] scanStructPackages;
+    private final String[] scanStructPackages;
 
     public StructSerializerManagement(String... scanStructPackages) {
         this.scanStructPackages = scanStructPackages;
 
+        for (String scanStructPackage : distinct(this.scanStructPackages )) {
+            StructCache.doScan(scanStructPackage);
+        }
     }
 }
