3ba3a607eca93e838029655aeda9ab26612ff608
fengbinbin <290361638@qq.com>
2024-01-24 16:17:53 +0800

    feat(update): c type
    add c type

--- src/main/java/org/fz/nettyx/serializer/struct/StructUtils.java
+++ src/main/java/org/fz/nettyx/serializer/struct/StructUtils.java
@@ -2,7 +2,10 @@ package org.fz.nettyx.serializer.struct;
 
 import cn.hutool.core.annotation.AnnotationUtil;
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.exceptions.NotInitedException;
+import cn.hutool.core.lang.ClassScanner;
 import cn.hutool.core.lang.reflect.MethodHandleUtil;
+import cn.hutool.core.map.WeakConcurrentMap;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.ReflectUtil;
 import io.netty.buffer.ByteBuf;
@@ -11,17 +14,23 @@ import io.netty.buffer.Unpooled;
 import lombok.experimental.UtilityClass;
 import org.fz.nettyx.exception.SerializeException;
 import org.fz.nettyx.exception.TooLessBytesException;
+import org.fz.nettyx.serializer.struct.annotation.Struct;
 import org.fz.nettyx.serializer.struct.basic.Basic;
 import org.fz.nettyx.util.Try;
 
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
 
 import static org.fz.nettyx.serializer.struct.PropertyHandler.*;
 import static org.fz.nettyx.serializer.struct.StructSerializer.isStruct;
-import static org.fz.nettyx.serializer.struct.StructCache.*;
+import static org.fz.nettyx.serializer.struct.StructUtils.StructCache.*;
 
 
 /**
@@ -217,4 +226,84 @@ public class StructUtils {
         return ReflectUtil.getFields(clazz, f -> !Modifier.isStatic(f.getModifiers()));
     }
 
+    /**
+     * The type Struct cache.
+     */
+    static final class StructCache {
+
+        /* reflection cache */
+        static final Map<Field, Method> FIELD_READER_CACHE = new WeakConcurrentMap<>();
+        static final Map<Field, Method> FIELD_WRITER_CACHE = new WeakConcurrentMap<>();
+
+        static final Map<Class<? extends Basic<?>>, Integer> BASIC_BYTES_SIZE_CACHE = new WeakConcurrentMap<>();
+
+        /* mapping handler and annotation */
+        static final Map<Class<? extends Annotation>, Class<? extends PropertyHandler<? extends Annotation>>> ANNOTATION_HANDLER_MAPPING_CACHE = new ConcurrentHashMap<>();
+
+        static {
+            try {
+                Set<Class<?>> classes = ClassScanner.scanAllPackage();
+
+                scanAllHandlers(classes);
+                scanAllBasics(classes);
+                scanAllStructs(classes);
+            } catch (Exception e) {
+                throw new NotInitedException("init serializer context failed please check", e);
+            }
+        }
+
+        private static synchronized void scanAllHandlers(Set<Class<?>> classes) {
+            for (Class<?> clazz : classes) {
+                boolean isPropertyHandler = PropertyHandler.class.isAssignableFrom(clazz) && !PropertyHandler.class.equals(clazz);
+                if (isPropertyHandler) {
+                    Class<Annotation> targetAnnotationType = getTargetAnnotationType(clazz);
+                    if (targetAnnotationType != null) {
+                        ANNOTATION_HANDLER_MAPPING_CACHE.putIfAbsent(targetAnnotationType,
+                                (Class<? extends PropertyHandler<? extends Annotation>>) clazz);
+
+                        ReflectUtil.getConstructor(clazz);
+                    }
+                }
+            }
+        }
+
+        private static synchronized void scanAllBasics(Set<Class<?>> classes)
+                throws InvocationTargetException, InstantiationException, IllegalAccessException {
+            for (Class<?> clazz : classes) {
+                int mod;
+                boolean isBasic =
+                        Basic.class.isAssignableFrom(clazz)
+                        && !Basic.class.equals(clazz)
+                        && !clazz.isEnum()
+                        && !Modifier.isAbstract((mod = clazz.getModifiers()))
+                        && !Modifier.isInterface(mod);
+                if (isBasic) {
+                    BASIC_BYTES_SIZE_CACHE.putIfAbsent((Class<? extends Basic<?>>) clazz,
+                            Basic.reflectForSize((Class<? extends Basic<?>>) clazz));
+                }
+            }
+        }
+
+        private static synchronized void scanAllStructs(Set<Class<?>> classes) throws IntrospectionException {
+            for (Class<?> clazz : classes) {
+                if (AnnotationUtil.hasAnnotation(clazz, Struct.class)) {
+                    ReflectUtil.getConstructor(clazz);
+
+                    Field[] structFields = StructUtils.getStructFields(clazz);
+
+                    for (Field field : structFields) {
+                        PropertyDescriptor propertyDescriptor = new PropertyDescriptor(field.getName(), clazz);
+                        FIELD_READER_CACHE.putIfAbsent(field, propertyDescriptor.getReadMethod());
+                        FIELD_WRITER_CACHE.putIfAbsent(field, propertyDescriptor.getWriteMethod());
+
+                        AnnotationUtil.getAnnotations(field, false);
+                    }
+                }
+            }
+        }
+
+        private StructCache() {
+            throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+        }
+    }
 }
