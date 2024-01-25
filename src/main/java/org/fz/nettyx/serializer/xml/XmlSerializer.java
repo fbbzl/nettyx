183925b0edb6641597af3d3237b95a9c60673c26
fengbinbin <290361638@qq.com>
2024-01-25 17:09:21 +0800

    feat(update): c type
    add c type

--- src/main/java/org/fz/nettyx/serializer/xml/XmlSerializer.java
+++ src/main/java/org/fz/nettyx/serializer/xml/XmlSerializer.java
@@ -2,12 +2,15 @@ package org.fz.nettyx.serializer.xml;
 
 import io.netty.buffer.ByteBuf;
 import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.fz.nettyx.serializer.Serializer;
 
 import javax.xml.bind.JAXB;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.io.File;
 
 
 /**
@@ -17,13 +20,13 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @version 1.0
  * @since 2023/12/27 9:35
  */
+@Getter
+@RequiredArgsConstructor
 public class XmlSerializer implements Serializer {
 
-    @Override
-    public ByteBuf getByteBuf() {
-        return null;
-    }
+    private final ByteBuf byteBuf;
 
+    private final File xml;
 
     @Data
     @XmlAccessorType(XmlAccessType.FIELD)
@@ -38,9 +41,10 @@ public class XmlSerializer implements Serializer {
 
     public static void main(String[] args) {
         Student student = new Student();
-        student.setAge(1);
+        student.setAge(11111256);
         student.setName("bb");
 
-        JAXB.marshal(student, "C:\\Users\\fengbinbin\\Desktop\\ff.txt");
+        Student unmarshal = JAXB.unmarshal("C:\\Users\\fengbinbin\\Desktop\\ff.txt", Student.class);
+
     }
 }
