bb466ed7c667f5ffa1978a29c4e1739ef692e09c
fengbinbin <290361638@qq.com>
2024-01-25 15:57:39 +0800

    feat(update): c type
    add c type

--- src/main/java/org/fz/nettyx/serializer/xml/XmlSerializer.java
+++ src/main/java/org/fz/nettyx/serializer/xml/XmlSerializer.java
@@ -1,10 +1,18 @@
 package org.fz.nettyx.serializer.xml;
 
 import io.netty.buffer.ByteBuf;
+import lombok.Data;
 import org.fz.nettyx.serializer.Serializer;
+
+import javax.xml.bind.JAXB;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
 
 /**
  * the location of the xml file is scanned
+ *
  * @author fengbinbin
  * @version 1.0
  * @since 2023/12/27 9:35
@@ -14,5 +22,24 @@ public class XmlSerializer implements Serializer {
     @Override
     public ByteBuf getByteBuf() {
         return null;
+    }
+
+
+    @Data
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlRootElement
+    public static class Student {
+
+        private String name;
+
+        private int age;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setAge(1);
+        student.setName("bb");
+
+        JAXB.marshal(student, "C:\\Users\\fengbinbin\\Desktop\\ff.txt");
     }
 }
