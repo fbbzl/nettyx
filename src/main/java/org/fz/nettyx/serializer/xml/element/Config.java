c969ad1a8df44a50d991bb3725dc7b82e3f60f0c
fengbinbin <290361638@qq.com>
2024-01-26 10:24:14 +0800

    feat(update): c type
    add c type

--- src/main/java/org/fz/nettyx/serializer/xml/element/Config.java
+++ src/main/java/org/fz/nettyx/serializer/xml/element/Config.java
@@ -2,6 +2,8 @@ package org.fz.nettyx.serializer.xml.element;
 
 import lombok.Data;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -12,17 +14,23 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @Data
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Config {
 
     private Bytes[] bytes;
 
-
     @Data
     public static class Bytes {
+        private int index;
+        private int length;
 
+        private Bits bits;
     }
 
     @Data
     public static class Bits {
+
+        private boolean[] value;
+
     }
 }
