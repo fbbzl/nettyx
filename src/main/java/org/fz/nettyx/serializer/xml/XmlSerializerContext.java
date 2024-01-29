4a8dfa6e3e55afe148d49d82dc28b3fa324b9ee5
fengbinbin <290361638@qq.com>
2024-01-29 11:32:48 +0800

    feat(update): c type
    add c type

---
+++ src/main/java/org/fz/nettyx/serializer/xml/XmlSerializerContext.java
@@ -0,0 +1,16 @@
+package org.fz.nettyx.serializer.xml;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/1/29 11:16
+ */
+public class XmlSerializerContext {
+
+
+
+
+
+    public static class XmlScanner {
+    }
+}
