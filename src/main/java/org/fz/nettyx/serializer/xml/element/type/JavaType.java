5d46cfdc4d2d0f779ca1e72f593ee4bf55aba53a
fengbinbin <290361638@qq.com>
2024-01-29 14:52:03 +0800

    feat(update): c type
    add c type

---
+++ src/main/java/org/fz/nettyx/serializer/xml/element/type/JavaType.java
@@ -0,0 +1,15 @@
+package org.fz.nettyx.serializer.xml.element.type;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/1/29 14:44
+ */
+public enum JavaType {
+
+    STRING,
+    INT,
+    LONG,
+    FLOAT,
+    DOUBLE
+}
