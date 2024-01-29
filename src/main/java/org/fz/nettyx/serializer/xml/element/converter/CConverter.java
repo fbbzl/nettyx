bd2da2d795efbe2e080c6d8897b6fd8f1b17003d
fengbinbin <290361638@qq.com>
2024-01-29 17:22:24 +0800

    feat(update): c type
    add c type

--- src/main/java/org/fz/nettyx/serializer/xml/element/converter/CConverter.java
+++ src/main/java/org/fz/nettyx/serializer/xml/element/converter/CConverter.java
@@ -5,5 +5,5 @@ package org.fz.nettyx.serializer.xml.element.converter;
  * @version 1.0
  * @since 2024/1/29 14:54
  */
-public class CConverter {
+public class CConverter extends BytesConverter {
 }
