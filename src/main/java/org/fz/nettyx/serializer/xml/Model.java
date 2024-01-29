a70d8954b6139b204afdd5415ea909cdbb3d6c79
fengbinbin <290361638@qq.com>
2024-01-29 18:05:48 +0800

    feat(update): c type
    add c type

---
+++ src/main/java/org/fz/nettyx/serializer/xml/Model.java
@@ -0,0 +1,18 @@
+package org.fz.nettyx.serializer.xml;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/1/29 18:03
+ */
+public class Model {
+
+    private String namespace;
+
+
+
+
+
+
+
+}
