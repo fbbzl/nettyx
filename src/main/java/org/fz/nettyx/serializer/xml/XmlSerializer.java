1f5a7f2968615c2a021c24baacd35b779d5ce8b5
fengbinbin <290361638@qq.com>
2024-01-29 14:34:12 +0800

    feat(update): c type
    add c type

--- src/main/java/org/fz/nettyx/serializer/xml/XmlSerializer.java
+++ src/main/java/org/fz/nettyx/serializer/xml/XmlSerializer.java
@@ -5,11 +5,17 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
-import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 import org.fz.nettyx.serializer.Serializer;
+import org.fz.nettyx.serializer.xml.element.Element;
 
 import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.fz.nettyx.serializer.xml.Dtd.NAME;
+import static org.fz.nettyx.serializer.xml.Dtd.REF;
 
 
 /**
@@ -28,12 +34,17 @@ public class XmlSerializer implements Serializer {
     private final File xml;
 
     public static void main(String[] args) throws DocumentException {
+        Map<String, List<Element>> config = new LinkedHashMap<>(64);
+
         SAXReader reader = SAXReader.createDefault();
         Document document = reader.read(new File("C:\\Users\\fengbinbin\\Desktop\\bytes.txt"));
-        Element rootElement = document.getRootElement();
-        System.err.println(rootElement.attribute("ref").getValue());
-        for (Element element : rootElement.elements()) {
-            System.err.println(element.attribute("name").getValue());
+        org.dom4j.Element rootElement = document.getRootElement();
+        System.err.println(rootElement.attribute(REF).getValue());
+
+
+        for (org.dom4j.Element element : rootElement.elements()) {
+
+            System.err.println(element.attribute(NAME).getValue());
 
         }
 
--- src/main/java/org/fz/nettyx/serializer/xml/element/ElementHandler.java
+++
@@ -1,9 +0,0 @@
-package org.fz.nettyx.serializer.xml.element;
-
-/**
- * @author fengbinbin
- * @version 1.0
- * @since 2024/1/26 17:56
- */
-public interface ElementHandler {
-}
