9d18d38cb88e0ff607a32f833b43766a65647330
fengbinbin <290361638@qq.com>
2024-01-29 17:52:49 +0800

    feat(update): c type
    add c type

--- src/main/java/org/fz/nettyx/serializer/xml/XmlSerializerContext.java
+++ src/main/java/org/fz/nettyx/serializer/xml/XmlSerializerContext.java
@@ -1,11 +1,15 @@
 package org.fz.nettyx.serializer.xml;
 
+import java.util.Map;
+
 /**
  * @author fengbinbin
  * @version 1.0
  * @since 2024/1/29 11:16
  */
 public class XmlSerializerContext {
+
+    private static final Map<String, >
 
     private String[] path;
 
