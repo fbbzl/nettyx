4a21b4eb29a281e5dab2be2d7e1fe50de8bfae5d
fengbinbin <290361638@qq.com>
2024-01-30 16:26:51 +0800

    feat(update): c type
    add c type

--- src/main/java/org/fz/nettyx/serializer/xml/XmlSerializer.java
+++ src/main/java/org/fz/nettyx/serializer/xml/XmlSerializer.java
@@ -20,7 +20,6 @@ import java.io.File;
 public class XmlSerializer implements Serializer {
 
     private final ByteBuf byteBuf;
-
     private final File xml;
 
     public static void main(String[] args) {
