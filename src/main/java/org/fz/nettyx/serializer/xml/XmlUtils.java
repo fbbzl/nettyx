6f125b1a06ccb5564113e213faca5b6b969edc5d
fengbinbin <290361638@qq.com>
2024-01-31 22:17:28 +0800

    feat(xml): add xml serializer
    add xml serializer

--- src/main/java/org/fz/nettyx/serializer/xml/XmlUtils.java
+++ src/main/java/org/fz/nettyx/serializer/xml/XmlUtils.java
@@ -11,7 +11,7 @@ import java.util.function.Predicate;
 import static cn.hutool.core.text.CharSequenceUtil.EMPTY;
 import static cn.hutool.core.text.CharSequenceUtil.splitToArray;
 import static java.util.stream.Collectors.toCollection;
-import static org.fz.nettyx.serializer.xml.Dtd.ATTR_REF;
+import static org.fz.nettyx.serializer.xml.dtd.Dtd.ATTR_REF;
 
 
 /**
@@ -24,40 +24,49 @@ import static org.fz.nettyx.serializer.xml.Dtd.ATTR_REF;
 public class XmlUtils {
 
     public static String textTrim(Element element) {
-        if (element == null) return EMPTY;
+        if (element == null) {
+            return EMPTY;
+        }
         return element.getTextTrim();
     }
 
     public static String attrValue(Element element, String name) {
         Attribute attribute;
-        if (element == null || (attribute = element.attribute(name)) == null) return EMPTY;
+        if (element == null || (attribute = element.attribute(name)) == null) {
+            return EMPTY;
+        }
 
         return attribute.getValue();
     }
 
     public static List<Element> elements(Element element, String name, Predicate<Element> filter) {
-        if (element == null) return Collections.emptyList();
+        if (element == null) {
+            return Collections.emptyList();
+        }
         List<Element> elements = element.elements(name);
         elements.removeIf(filter.negate());
         return elements;
     }
 
     public static List<Element> elements(Element element, String name) {
-        if (element == null) return Collections.emptyList();
+        if (element == null) {
+            return Collections.emptyList();
+        }
         return element.elements(name);
     }
 
     public static void putConst(Element rootElement, String boundary, String name, Map<String, Set<String>> map) {
         for (Iterator<Element> it = rootElement.elementIterator(boundary); it.hasNext(); ) {
             for (Element enumEl : it.next().elements(name)) {
-                if (enumEl == null) continue;
+                if (enumEl == null) {
+                    continue;
+                }
 
                 String enumRef = enumEl.attribute(ATTR_REF).getValue();
 
-                map.putIfAbsent(enumRef, Arrays.stream(splitToArray(enumEl.getTextTrim(), ","))
-                        .map(CharSequenceUtil::removeAllLineBreaks)
-                        .map(CharSequenceUtil::cleanBlank)
-                        .collect(toCollection(LinkedHashSet::new)));
+                map.putIfAbsent(enumRef,
+                    Arrays.stream(splitToArray(enumEl.getTextTrim(), ",")).map(CharSequenceUtil::removeAllLineBreaks)
+                        .map(CharSequenceUtil::cleanBlank).collect(toCollection(LinkedHashSet::new)));
             }
         }
     }
--- src/main/java/org/fz/nettyx/serializer/xml/element/Model.java
+++ src/main/java/org/fz/nettyx/serializer/xml/element/Model.java
@@ -1,17 +1,15 @@
 package org.fz.nettyx.serializer.xml.element;
 
-import static org.fz.nettyx.serializer.xml.Dtd.ATTR_COUNTER_TYPE;
-import static org.fz.nettyx.serializer.xml.Dtd.ATTR_REF;
-import static org.fz.nettyx.serializer.xml.Dtd.EL_PROP;
-import static org.fz.nettyx.serializer.xml.element.Model.OffsetType.RELATIVE;
-
 import cn.hutool.core.util.EnumUtil;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.Data;
 import org.dom4j.Element;
-import org.dom4j.Namespace;
 import org.fz.nettyx.serializer.xml.XmlUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.fz.nettyx.serializer.xml.dtd.Dtd.*;
+import static org.fz.nettyx.serializer.xml.element.Model.OffsetType.RELATIVE;
 
 /**
  * @author fengbinbin
@@ -24,12 +22,12 @@ public class Model {
 
     private static final OffsetType DEFAULT_OFFSET_TYPE = RELATIVE;
 
-    private Namespace namespace;
+    private String namespace;
     private String ref;
     private OffsetType offsetType;
     private List<Prop> props;
 
-    public Model(Element modelEl, Namespace namespace) {
+    public Model(Element modelEl, String namespace) {
         this(modelEl);
         this.namespace = namespace;
     }
@@ -37,7 +35,7 @@ public class Model {
     public Model(Element modelEl) {
         this.ref = XmlUtils.attrValue(modelEl, ATTR_REF);
         this.offsetType = EnumUtil.fromString(OffsetType.class,
-            XmlUtils.attrValue(modelEl, ATTR_COUNTER_TYPE).toUpperCase(), RELATIVE);
+            XmlUtils.attrValue(modelEl, ATTR_OFFSET_TYPE).toUpperCase(), RELATIVE);
         this.props = XmlUtils.elements(modelEl, EL_PROP).stream().map(Prop::new).collect(Collectors.toList());
     }
 
