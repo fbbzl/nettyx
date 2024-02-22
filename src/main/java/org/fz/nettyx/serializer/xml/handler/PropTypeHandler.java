aee604fd6c45f7a285ec283192bd139fba53af67
fengbinbin <290361638@qq.com>
2024-02-22 13:56:12 +0800

    feat(xml): scripts engine
    add mvel2 scripts engine

--- src/main/java/org/fz/nettyx/serializer/xml/handler/PropTypeHandler.java
+++ src/main/java/org/fz/nettyx/serializer/xml/handler/PropTypeHandler.java
@@ -1,7 +1,7 @@
 package org.fz.nettyx.serializer.xml.handler;
 
 import io.netty.buffer.ByteBuf;
-import org.fz.nettyx.serializer.xml.element.Model.PropElement;
+import org.fz.nettyx.serializer.xml.dtd.Model.Prop;
 
 /**
  * The interface Type handler.
@@ -10,19 +10,15 @@ import org.fz.nettyx.serializer.xml.element.Model.PropElement;
  * @version 1.0
  * @since 2024 /2/7 10:51
  */
-public interface XmlPropHandler {
+public interface PropTypeHandler extends PropHandler {
 
     /**
      * the type string value handled
      */
     String forType();
 
-    String read(PropElement prop, ByteBuf reading);
-
-    void write(PropElement prop, ByteBuf writing);
-
-    default byte[] readBytes(PropElement prop, ByteBuf byteBuf) {
-        // TODO 相对位置 和 绝对位置
+    default byte[] readBytes(Prop prop, ByteBuf byteBuf) {
+        // TODO  相对位置 和 绝对位置
         byte[] bytes = new byte[prop.getLength()];
         byteBuf.readBytes(bytes);
         return bytes;
