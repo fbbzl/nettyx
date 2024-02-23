9edbd95e51dbc9a9bd1322b0d0e2c5e7bbe2c2e8
fengbinbin <290361638@qq.com>
2024-02-23 09:43:41 +0800

    feat(openssl): add openssl extend code

---
+++ src/main/java/org/fz/nettyx/ssl/OpenSslInitializer.java
@@ -0,0 +1,26 @@
+package org.fz.nettyx.ssl;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/2/24 20:12
+ */
+
+@Slf4j
+public class OpenSslInitializer {
+
+    public void initCaRoot() {
+
+    }
+
+    public void initClient() {
+
+    }
+
+    public void initServer() {
+
+    }
+
+}
