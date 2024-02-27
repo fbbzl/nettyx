ddec3c1e53ea05c33df58cb83035501e398fefc2
fengbinbin <290361638@qq.com>
2024-02-27 10:26:42 +0800

    feat(jsc): add jsc channel

--- src/main/java/org/fz/nettyx/exception/FieldHandlerException.java
+++ src/main/java/org/fz/nettyx/exception/FieldHandlerException.java
@@ -1,7 +1,8 @@
 package org.fz.nettyx.exception;
 
-import java.lang.reflect.Field;
 import lombok.Getter;
+
+import java.lang.reflect.Field;
 
 /**
  * @author fengbinbin
@@ -9,13 +10,13 @@ import lombok.Getter;
  * @since 2024/1/11 16:02
  */
 @Getter
-public class HandlerException extends RuntimeException {
+public class FieldHandlerException extends RuntimeException {
 
     private final transient Field field;
 
     private final Class<?> handlerClass;
 
-    public HandlerException(Field field, Class<?> handlerClass, Throwable throwable) {
+    public FieldHandlerException(Field field, Class<?> handlerClass, Throwable throwable) {
         super("handler cause exception please check [" + handlerClass + "], this handler is on field [" + field + "]", throwable);
         this.field = field;
         this.handlerClass = handlerClass;
