11de25ddf06f54bc21b826cd5f975b965be54992
fengbinbin <290361638@qq.com>
2024-02-29 18:26:24 +0800

    feat(upgrade): 2.2.2-RELEASE

--- src/test/java/client/TestMultiTcp.java
+++ src/test/java/client/TestMultiTcp.java
@@ -2,12 +2,15 @@ package client;
 
 import cn.hutool.core.lang.Console;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import org.fz.nettyx.endpoint.tcp.client.MultiTcpChannelClient;
 import org.fz.nettyx.listener.ActionableChannelFutureListener;
 
 import java.net.SocketAddress;
+import java.util.Map;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -18,7 +21,16 @@ import static java.util.concurrent.TimeUnit.SECONDS;
  */
 public class TestMultiTcp extends MultiTcpChannelClient<String> {
 
+    protected TestMultiTcp(Map<String, SocketAddress> stringSocketAddressMap) {
+        super(stringSocketAddressMap);
+    }
+
     @Override
+    protected ChannelInitializer<? extends Channel> channelInitializer(String key) {
+        return null;
+    }
+
+
     protected void connect(String key, SocketAddress address) {
         Console.log("connecting address [" + address.toString() + "]");
         ChannelFutureListener listener = new ActionableChannelFutureListener()
