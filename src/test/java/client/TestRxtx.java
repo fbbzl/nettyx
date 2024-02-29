18a5b490de82358fd2a0925b21b7457d3e65a751
fengbinbin <290361638@qq.com>
2024-02-29 10:36:42 +0800

    feat(upgrade): 2.2.2-RELEASE

--- src/test/java/client/TestRxtx.java
+++ src/test/java/client/TestRxtx.java
@@ -1,9 +1,30 @@
 package client;
 
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.rxtx.RxtxChannelConfig;
+import io.netty.channel.rxtx.RxtxDeviceAddress;
+import org.fz.nettyx.endpoint.serial.rxtx.SingleRxtxChannelClient;
+
 /**
  * @author fengbinbin
  * @version 1.0
  * @since 2024/2/29 10:31
  */
 public class TestRxtx {
+
+
+    public static void main(String[] args) {
+        SingleRxtxChannelClient singleRxtxChannelClient = new SingleRxtxChannelClient() {
+            @Override
+            public ChannelFuture connect(RxtxDeviceAddress address) throws Exception {
+                return null;
+            }
+
+            @Override
+            protected void doRxtxConfig(RxtxChannelConfig rxtxChannel) {
+
+            }
+        };
+
+    }
 }
