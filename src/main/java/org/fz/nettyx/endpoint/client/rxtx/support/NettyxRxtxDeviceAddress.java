fa0525e04a2711a7d8b6ae35aab17fc43b03dd71
fengbinbin <290361638@qq.com>
2024-03-04 20:48:46 +0800

    feat(upgrade): 2.2.2-RELEASE

---
+++ src/main/java/org/fz/nettyx/endpoint/client/rxtx/support/NettyxRxtxDeviceAddress.java
@@ -0,0 +1,27 @@
+package org.fz.nettyx.endpoint.client.rxtx.support;
+
+import io.netty.channel.rxtx.RxtxDeviceAddress;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/3/4 20:47
+ */
+public class NettyxRxtxDeviceAddress extends RxtxDeviceAddress {
+
+    /**
+     * Creates a RxtxDeviceAddress representing the address of the serial port.
+     *
+     * @param value the address of the device (e.g. COM1, /dev/ttyUSB0, ...)
+     */
+    public NettyxRxtxDeviceAddress(String value) {
+        super(value);
+    }
+
+    @Override
+    public String toString() {
+        return "RxtxDeviceAddress{" +
+               "value='" + value() + '\'' +
+               '}';
+    }
+}
