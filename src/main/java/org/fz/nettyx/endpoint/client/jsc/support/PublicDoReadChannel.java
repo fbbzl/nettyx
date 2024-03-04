0df8a915d13d7d510d81867836e278d094db5cc2
fengbinbin <290361638@qq.com>
2024-03-04 11:44:57 +0800

    feat(upgrade): 2.2.2-RELEASE

--- src/main/java/org/fz/nettyx/endpoint/client/jsc/support/PublicDoReadChannel.java
+++ src/main/java/org/fz/nettyx/endpoint/client/jsc/support/PublicDoReadChannel.java
@@ -3,9 +3,16 @@ package org.fz.nettyx.endpoint.client.jsc.support;
 import io.netty.channel.Channel;
 import io.netty.channel.oio.OioByteStreamChannel;
 
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/3/2 13:29
+ */
+@SuppressWarnings("deprecation")
 abstract class PublicDoReadChannel extends OioByteStreamChannel {
 
-    public PublicDoReadChannel(Channel parent) {
+    protected PublicDoReadChannel(Channel parent) {
         super(parent);
     }
 
