7eca07a770c0ff30f1fd7e1ed1ba33acdbb23bcd
fengbinbin <290361638@qq.com>
2024-03-04 22:20:47 +0800

    feat(upgrade): 2.2.3-RELEASE

--- src/test/java/codec/UserCodec.java
+++ src/test/java/codec/UserCodec.java
@@ -10,15 +10,24 @@ import model.User;
 import model.Wife;
 import org.fz.nettyx.serializer.struct.TypeRefer;
 import org.fz.nettyx.serializer.struct.basic.c.signed.Clong4;
+import org.fz.nettyx.util.HexKit;
 
 @Slf4j
 public class UserCodec extends SimpleChannelInboundHandler<ByteBuf> {
+
     TypeRefer<User<Son<Clong4, Clong4>, Wife, GirlFriend>> typeRefer = new TypeRefer<User<Son<Clong4, Clong4>, Wife, GirlFriend>>() {
     };
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) {
         System.err.println(msg.readableBytes());
+
+        if (msg.readableBytes() == 8) {
+            ctx.channel().writeAndFlush(HexKit.decode("ffffffff"));
+        }
+        if (msg.readableBytes() == 4) {
+            ctx.channel().writeAndFlush(HexKit.decode("ffffffffffffffff"));
+        }
         msg.release();
     }
 }
