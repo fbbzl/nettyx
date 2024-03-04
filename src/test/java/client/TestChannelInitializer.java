7d4f9600ef6a566bbc8cb3955420429f5500521b
fengbinbin <290361638@qq.com>
2024-03-04 22:16:52 +0800

    feat(upgrade): 2.2.3-RELEASE

--- src/test/java/client/TestChannelInitializer.java
+++ src/test/java/client/TestChannelInitializer.java
@@ -1,5 +1,7 @@
 package client;
 
+import static io.netty.buffer.Unpooled.wrappedBuffer;
+
 import codec.UserCodec;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
@@ -8,8 +10,6 @@ import org.fz.nettyx.codec.EscapeCodec;
 import org.fz.nettyx.codec.EscapeCodec.EscapeMap;
 import org.fz.nettyx.codec.StartEndFlagFrameCodec;
 import org.fz.nettyx.handler.LoggerHandler;
-
-import static io.netty.buffer.Unpooled.wrappedBuffer;
 
 /**
  * @author fengbinbin
@@ -22,14 +22,10 @@ public class TestChannelInitializer<C extends Channel> extends ChannelInitialize
 
     @Override
     protected void initChannel(C channel) {
-
-
         channel.pipeline().addLast(
-
-            new StartEndFlagFrameCodec(false, wrappedBuffer(new byte[]{(byte) 0x7e}))
+            new StartEndFlagFrameCodec(true, wrappedBuffer(new byte[]{(byte) 0x7e}))
             , new EscapeCodec(EscapeMap.mapHex("7e", "7d5e"))
-             , new UserCodec()
-            , new LoggerHandler(log)
-            );
+            , new UserCodec()
+            , new LoggerHandler(log));
     }
 }
