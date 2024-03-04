1c8e6fa2160898041c99bc3ec22807aac0a6c164
fengbinbin <290361638@qq.com>
2024-03-04 20:15:48 +0800

    feat(upgrade): 2.2.2-RELEASE

--- src/main/java/org/fz/nettyx/endpoint/client/AbstractSingleChannelClient.java
+++ src/main/java/org/fz/nettyx/endpoint/client/AbstractSingleChannelClient.java
@@ -76,18 +76,14 @@ public abstract class AbstractSingleChannelClient<C extends Channel> extends
     }
 
     public ChannelPromise writeAndFlush(Object message) {
-        if (this.notActive(channel)) {
-            log.debug("comm channel not in active status, message will be discard: {}", message);
+        if (this.notActive(channel) || notWritable(channel)) {
+            log.debug("comm channel not in usable status, message will be discard: {}", message);
             ReferenceCountUtil.safeRelease(message);
             return failurePromise(channel, "comm channel: [" + channel + "] is not usable");
         }
 
         try {
-            if (notWritable(channel)) {
-                log.debug("comm channel [{}] is not writable", channel);
-                ReferenceCountUtil.safeRelease(message);
-                return failurePromise(channel, "comm channel: [" + channel + "] is not writable");
-            } else { return (ChannelPromise) channel.writeAndFlush(message); }
+            return (ChannelPromise) channel.writeAndFlush(message);
         }
         catch (Exception exception) {
             throw new ChannelException("exception occurred while sending the message [" + message + "], comm-port is ["
