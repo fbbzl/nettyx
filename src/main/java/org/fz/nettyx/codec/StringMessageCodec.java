77d2d160e603a23887dbc2825c9c657502dd23ca
fengbinbin <290361638@qq.com>
2024-03-15 14:01:34 +0800

    feat(upgrade): 2.2.5-RELEASE

--- src/main/java/org/fz/nettyx/codec/StringMessageCodec.java
+++ src/main/java/org/fz/nettyx/codec/StringMessageCodec.java
@@ -4,10 +4,11 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageCodec;
 import io.netty.util.ReferenceCountUtil;
-import java.nio.charset.Charset;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * basic string codec
@@ -23,15 +24,16 @@ public class StringMessageCodec extends ByteToMessageCodec<String> {
     private final Charset charset;
 
     @Override
-    protected void encode(ChannelHandlerContext channelHandlerContext, String msg, ByteBuf byteBuf) {
-        byteBuf.writeBytes(msg.getBytes(charset));
-    }
-
-    @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) {
         list.add(byteBuf.toString(charset));
 
         // release after to string
         ReferenceCountUtil.release(byteBuf);
     }
+
+    @Override
+    protected void encode(ChannelHandlerContext channelHandlerContext, String msg, ByteBuf byteBuf) {
+        byteBuf.writeBytes(msg.getBytes(charset));
+    }
+
 }
