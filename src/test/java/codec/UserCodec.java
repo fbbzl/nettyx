a93c935415c1b2e6acf1b7451849ae661f52dbf8
fengbinbin <290361638@qq.com>
2024-03-19 10:22:57 +0800

    feat(upgrade): 2.2.5-RELEASE

--- src/test/java/codec/UserCodec.java
+++ src/test/java/codec/UserCodec.java
@@ -2,33 +2,31 @@ package codec;
 
 import cn.hutool.core.date.StopWatch;
 import cn.hutool.core.lang.Console;
-import codec.model.GirlFriend;
-import codec.model.Son;
 import codec.model.User;
 import codec.model.Wife;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.fz.nettyx.serializer.struct.StructSerializer;
 import org.fz.nettyx.serializer.struct.TypeRefer;
 import org.fz.nettyx.serializer.struct.basic.c.signed.Clong4;
 
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 public class UserCodec extends SimpleChannelInboundHandler<ByteBuf> {
 
-    TypeRefer<User<Son<Clong4, Clong4>, Wife, GirlFriend>> typeRefer = new TypeRefer<User<Son<Clong4, Clong4>, Wife,
-            GirlFriend>>() {
+    TypeRefer<User<Clong4, Wife>> typeRefer = new TypeRefer<User<Clong4, Wife>>() {
     };
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) {
         StopWatch s = new StopWatch();
         s.start("read");
-        User<Son<Clong4, Clong4>, Wife, GirlFriend> read = StructSerializer.read(msg, typeRefer);
+        User read = StructSerializer.read(msg, typeRefer);
         s.stop();
-
+        System.err.println(read);
         Console.print(s.prettyPrint(TimeUnit.MILLISECONDS));
 
     }
