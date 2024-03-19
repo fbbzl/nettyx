dceccbc58a0d71853e08ec2d9109c68bf23f1bf1
fengbinbin <290361638@qq.com>
2024-03-19 21:11:33 +0800

    feat(upgrade): 2.2.8-RELEASE

--- src/test/java/codec/UserCodec.java
+++ src/test/java/codec/UserCodec.java
@@ -8,12 +8,11 @@ import codec.model.Wife;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.fz.nettyx.serializer.struct.StructSerializer;
 import org.fz.nettyx.serializer.struct.TypeRefer;
 import org.fz.nettyx.serializer.struct.basic.c.signed.Clong4;
-
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class UserCodec extends SimpleChannelInboundHandler<ByteBuf> {
@@ -25,7 +24,7 @@ public class UserCodec extends SimpleChannelInboundHandler<ByteBuf> {
     protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) {
         StopWatch s = new StopWatch();
         s.start("read");
-        User<Clong4, Wife, GirlFriend> read = StructSerializer.read(msg, typeRefer);
+        User<Clong4, Wife, GirlFriend> read = StructSerializer.read(typeRefer, msg);
         s.stop();
         System.err.println(read);
         Console.print(s.prettyPrint(TimeUnit.MILLISECONDS));
