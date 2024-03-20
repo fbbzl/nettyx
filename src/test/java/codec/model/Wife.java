f276b1f06746ee2c773efdc69a651f0d35ac24ed
fengbinbin <290361638@qq.com>
2024-03-20 08:09:30 +0800

    feat(upgrade): 2.2.9-RELEASE

--- src/test/java/codec/model/Wife.java
+++ src/test/java/codec/model/Wife.java
@@ -6,8 +6,9 @@ import org.fz.nettyx.serializer.struct.annotation.ToString;
 
 @Data
 @Struct
-public class Wife<V> {
+public class Wife<I, V> {
 
+    private I intt;
     @ToString(bufferLength = 2)
     private String name;
 
