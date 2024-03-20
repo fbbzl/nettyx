da7435a5e1198c92f4a40607a1dc521d79ef5137
fengbinbin <290361638@qq.com>
2024-03-20 08:09:44 +0800

    feat(upgrade): 2.2.9-RELEASE

--- src/test/java/SerializerTester.java
+++ src/test/java/SerializerTester.java
@@ -51,11 +51,11 @@ public class SerializerTester {
     public void testStructSerializer() {
         byte[] bytes = new byte[1024 * 1024];
         Arrays.fill(bytes, (byte) 67);
-        TypeRefer<User<Son<Cchar, Wife<Cint,Bill>>, Clong4, GirlFriend >> typeRefer = new TypeRefer<User<Son<Cchar, Wife<Bill>>, Clong4
+        TypeRefer<User<Son<Cchar, Wife<Cint,Bill>>, Clong4, GirlFriend >> typeRefer = new TypeRefer<User<Son<Cchar, Wife<Cint,Bill>>, Clong4
                 , GirlFriend>>() {
         };
 
-        User<Son<Cchar, Wife<Bill>>, Clong4, GirlFriend> user = StructSerializer.read(typeRefer,
+        User<Son<Cchar, Wife<Cint,Bill>>, Clong4, GirlFriend> user = StructSerializer.read(typeRefer,
                                                                                 Unpooled.wrappedBuffer(bytes));
 
         System.err.println("read :" + user);
