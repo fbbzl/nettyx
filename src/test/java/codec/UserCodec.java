36ecaa4b217808633213500fbd2a75f1badc47cf
fengbinbin <290361638@qq.com>
2024-03-21 14:12:59 +0800

    feat(upgrade): 2.2.5-RELEASE

--- src/test/java/codec/UserCodec.java
+++ src/test/java/codec/UserCodec.java
@@ -14,6 +14,7 @@ import org.fz.nettyx.serializer.struct.basic.c.unsigned.Culong8;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.fz.nettyx.endpoint.client.AbstractMultiChannelClient.channelKey;
 
 
 @Slf4j
