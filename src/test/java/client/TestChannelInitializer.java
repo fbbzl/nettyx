9774a71dc64a59d977c625d93f18e3a953c05bd1
fengbinbin <290361638@qq.com>
2024-03-23 09:23:23 +0800

    feat(upgrade): 2.2.9-RELEASE

--- src/test/java/client/TestChannelInitializer.java
+++ src/test/java/client/TestChannelInitializer.java
@@ -1,5 +1,8 @@
 package client;
 
+import static io.netty.buffer.Unpooled.wrappedBuffer;
+import static org.fz.nettyx.handler.LoggerHandler.Sl4jLevel.INFO;
+
 import codec.UserCodec;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
@@ -10,9 +13,6 @@ import org.fz.nettyx.codec.StartEndFlagFrameCodec;
 import org.fz.nettyx.handler.ChannelAdvice.InboundAdvice;
 import org.fz.nettyx.handler.ChannelAdvice.OutboundAdvice;
 import org.fz.nettyx.handler.LoggerHandler;
-
-import static io.netty.buffer.Unpooled.wrappedBuffer;
-import static org.fz.nettyx.handler.LoggerHandler.Sl4jLevel.INFO;
 
 /**
  * @author fengbinbin
@@ -32,7 +32,7 @@ public class TestChannelInitializer<C extends Channel> extends ChannelInitialize
 
         channel.pipeline().addLast(
                 outboundAdvice
-                , new StartEndFlagFrameCodec(1024 * 6, true, wrappedBuffer(new byte[]{(byte) 0x7e}))
+                , new StartEndFlagFrameCodec(256, true, wrappedBuffer(new byte[]{(byte) 0x7e}))
                 , new EscapeCodec(EscapeMap.mapHex("7e", "7d5e"))
                 , new UserCodec()
                 , new LoggerHandler(log, INFO)
--- src/test/java/client/rxtx/TestSingleRxtx.java
+++ src/test/java/client/rxtx/TestSingleRxtx.java
@@ -41,7 +41,7 @@ public class TestSingleRxtx extends SingleRxtxChannelClient {
     protected ChannelFutureAction whenConnectSuccess() {
         return cf -> {
             executor.scheduleAtFixedRate(() -> {
-                byte[] msg = new byte[1024 * 3];
+                byte[] msg = new byte[128];
                 Arrays.fill(msg, (byte) 1);
                 this.writeAndFlush(Unpooled.wrappedBuffer(msg));
             }, 2, 100, TimeUnit.MILLISECONDS);
