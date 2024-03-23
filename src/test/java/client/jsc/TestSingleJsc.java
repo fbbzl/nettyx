da3131bbf13807bf9c0a2e029723f8edaff2b5c0
fengbinbin <290361638@qq.com>
2024-03-23 13:32:58 +0800

    feat(upgrade): 2.2.9-RELEASE

--- src/test/java/client/jsc/TestSingleJsc.java
+++ src/test/java/client/jsc/TestSingleJsc.java
@@ -1,6 +1,8 @@
 package client.jsc;
 
 
+import static io.netty.channel.rxtx.RxtxChannelOption.DTR;
+import static io.netty.channel.rxtx.RxtxChannelOption.RTS;
 import static org.fz.nettyx.endpoint.client.jsc.support.JscChannelOption.BAUD_RATE;
 import static org.fz.nettyx.endpoint.client.jsc.support.JscChannelOption.DATA_BITS;
 import static org.fz.nettyx.endpoint.client.jsc.support.JscChannelOption.PARITY_BIT;
@@ -10,18 +12,16 @@ import client.TestChannelInitializer;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelInitializer;
-import java.net.SocketAddress;
 import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
-import org.fz.nettyx.action.ChannelFutureAction;
 import org.fz.nettyx.endpoint.client.jsc.SingleJscChannelClient;
 import org.fz.nettyx.endpoint.client.jsc.support.JscChannel;
 import org.fz.nettyx.endpoint.client.jsc.support.JscChannelConfig.ParityBit;
 import org.fz.nettyx.endpoint.client.jsc.support.JscChannelConfig.StopBits;
-import org.fz.nettyx.endpoint.client.jsc.support.JscDeviceAddress;
+import org.fz.nettyx.listener.ActionableChannelFutureListener;
 
 /**
  * @author fengbinbin
@@ -32,62 +32,46 @@ import org.fz.nettyx.endpoint.client.jsc.support.JscDeviceAddress;
 @Slf4j
 public class TestSingleJsc extends SingleJscChannelClient {
 
-    ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-
-
-    @Override
-    protected ChannelFutureAction whenConnectSuccess() {
-        return cf -> {
-            executor.scheduleAtFixedRate(() -> {
-                byte[] msg = new byte[300];
-                Arrays.fill(msg, (byte) 1);
-                this.writeAndFlush(Unpooled.wrappedBuffer(msg));
-            }, 2, 30, TimeUnit.MILLISECONDS);
-
-            System.err.println(cf.channel().localAddress() + ": ok");
-        };
-    }
-
-    @Override
-    protected ChannelFutureAction whenConnectFailure() {
-        return cf -> {
-            System.err.println(cf.channel().localAddress() + ": fail, " + cf.cause());
-            cf.channel().eventLoop().schedule(this::connect, 2, TimeUnit.SECONDS);
-        };
-    }
-
-    @Override
-    protected ChannelFutureAction whenConnectDone() {
-        return cf -> System.err.println("done");
-    }
-
-    @Override
-    protected ChannelFutureAction whenConnectCancel() {
-        return cf -> System.err.println("cancel");
-    }
+    static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
     @Override
     protected ChannelInitializer<JscChannel> channelInitializer() {
         return new TestChannelInitializer<>();
     }
 
-    @Override
-    protected void doChannelConfig(JscChannel channel) {
-        super.doChannelConfig(channel);
-    }
 
     @Override
-    protected Bootstrap newBootstrap(SocketAddress remoteAddress) {
-        return super.newBootstrap(remoteAddress)
+    protected Bootstrap newBootstrap() {
+        return super.newBootstrap()
                     .option(BAUD_RATE, 115200)
                     .option(DATA_BITS, 8)
                     .option(STOP_BITS, StopBits.ONE_STOP_BIT)
-                    .option(PARITY_BIT, ParityBit.NO_PARITY);
+                    .option(PARITY_BIT, ParityBit.NO_PARITY)
+                    .option(DTR, false)
+                    .option(RTS, false);
     }
 
     public static void main(String[] args) {
         TestSingleJsc testSingleJsc = new TestSingleJsc();
-        testSingleJsc.connect(new JscDeviceAddress("COM2"));
+        ActionableChannelFutureListener listener = new ActionableChannelFutureListener()
+            .whenSuccess(cf -> {
+                executor.scheduleAtFixedRate(() -> {
+                    byte[] msg = new byte[300];
+                    Arrays.fill(msg, (byte) 1);
+                    testSingleJsc.writeAndFlush(Unpooled.wrappedBuffer(msg));
+                }, 2, 30, TimeUnit.MILLISECONDS);
+
+                System.err.println(cf.channel().localAddress() + ": ok");
+            })
+            .whenCancel(cf -> System.err.println("cancel"))
+            .whenFailure(cf -> {
+                System.err.println(cf.channel().localAddress() + ": fail, " + cf.cause());
+                cf.channel().eventLoop()
+                  .schedule(() -> testSingleJsc.connect(cf.channel().remoteAddress()), 2, TimeUnit.SECONDS);
+            })
+            .whenDone(cf -> System.err.println("done"));
+
+        testSingleJsc.connect("COM2").addListener(listener);
     }
 
 }
