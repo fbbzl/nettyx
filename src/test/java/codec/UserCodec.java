b680882a084b2cc83dda0dd6885e3b614ad5f699
fengbinbin <290361638@qq.com>
2024-03-23 09:32:28 +0800

    feat(upgrade): 2.2.9-RELEASE

--- src/test/java/codec/UserCodec.java
+++ src/test/java/codec/UserCodec.java
@@ -1,20 +1,23 @@
 package codec;
 
+import static org.fz.nettyx.endpoint.client.AbstractMultiChannelClient.channelKey;
+
 import cn.hutool.core.date.StopWatch;
 import cn.hutool.core.lang.Console;
-import codec.model.*;
+import codec.model.Bill;
+import codec.model.GirlFriend;
+import codec.model.Son;
+import codec.model.User;
+import codec.model.Wife;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.fz.nettyx.serializer.struct.StructSerializer;
 import org.fz.nettyx.serializer.struct.TypeRefer;
 import org.fz.nettyx.serializer.struct.basic.c.signed.Clong4;
 import org.fz.nettyx.serializer.struct.basic.c.unsigned.Culong8;
-
-import java.util.concurrent.TimeUnit;
-
-import static org.fz.nettyx.endpoint.client.AbstractMultiChannelClient.channelKey;
 
 
 @Slf4j
@@ -31,7 +34,7 @@ public class UserCodec extends SimpleChannelInboundHandler<ByteBuf> {
         User<Clong4, Wife<Culong8, Son<Clong4, Bill>>, GirlFriend> read = StructSerializer.read(typeRefer, msg);
         s.stop();
 
-        Console.print(s.prettyPrint(TimeUnit.MILLISECONDS));
+        Console.print(s.prettyPrint(TimeUnit.NANOSECONDS));
 
     }
 }
