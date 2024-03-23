80cd4fe45a926813c4574c0f4e4dfe141cea4322
fengbinbin <290361638@qq.com>
2024-03-23 13:21:41 +0800

    feat(upgrade): 2.2.9-RELEASE

--- src/main/java/org/fz/nettyx/endpoint/client/tcp/SingleTcpChannelClient.java
+++ src/main/java/org/fz/nettyx/endpoint/client/tcp/SingleTcpChannelClient.java
@@ -4,7 +4,6 @@ package org.fz.nettyx.endpoint.client.tcp;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import java.net.InetSocketAddress;
 import lombok.extern.slf4j.Slf4j;
 import org.fz.nettyx.endpoint.client.AbstractSingleChannelClient;
 
@@ -16,12 +15,7 @@ import org.fz.nettyx.endpoint.client.AbstractSingleChannelClient;
  * @since 2021 /5/6 15:22
  */
 @Slf4j
-public abstract class SingleTcpChannelClient extends
-                                             AbstractSingleChannelClient<NioSocketChannel> {
-
-    protected SingleTcpChannelClient(InetSocketAddress address) {
-        super(address);
-    }
+public abstract class SingleTcpChannelClient extends AbstractSingleChannelClient<NioSocketChannel> {
 
     @Override
     protected EventLoopGroup newEventLoopGroup() {
