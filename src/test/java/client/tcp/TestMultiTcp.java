fc55c5f56c0cfd0e567a6675f605d936a5a11e0f
fengbinbin <290361638@qq.com>
2024-03-26 09:32:29 +0800

    test(test): add spring test

--- src/test/java/client/tcp/TestMultiTcp.java
+++ src/test/java/client/tcp/TestMultiTcp.java
@@ -7,6 +7,9 @@ import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannelConfig;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import org.fz.nettyx.endpoint.client.tcp.MultiTcpChannelClient;
+import org.fz.nettyx.listener.ActionChannelFutureListener;
+
 import java.net.InetSocketAddress;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -14,8 +17,6 @@ import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import org.fz.nettyx.endpoint.client.tcp.MultiTcpChannelClient;
-import org.fz.nettyx.listener.ActionChannelFutureListener;
 
 /**
  * @author fengbinbin
@@ -43,26 +44,26 @@ public class TestMultiTcp extends MultiTcpChannelClient<String> {
     public static void main(String[] args) {
         Map<String, InetSocketAddress> map = new HashMap<>();
 
-        map.put("a", new InetSocketAddress("127.0.0.1", 9081));
-        map.put("b", new InetSocketAddress("127.0.0.1", 9081));
+        map.put("a", new InetSocketAddress(9888));
+        map.put("b", new InetSocketAddress(9888));
 
         TestMultiTcp testMultiTcp = new TestMultiTcp(map);
         ChannelFutureListener listener = new ActionChannelFutureListener()
-            .whenSuccess(cf -> {
-                executor.scheduleAtFixedRate(() -> {
-                    byte[] msg = new byte[300];
-                    Arrays.fill(msg, (byte) 1);
-                    cf.channel().writeAndFlush(Unpooled.wrappedBuffer(msg));
-                }, 2, 30, TimeUnit.MILLISECONDS);
+                .whenSuccess(cf -> {
+                    executor.scheduleAtFixedRate(() -> {
+                        byte[] msg = new byte[300];
+                        Arrays.fill(msg, (byte) 1);
+                        cf.channel().writeAndFlush(Unpooled.wrappedBuffer(msg));
+                    }, 2, 30, TimeUnit.MILLISECONDS);
 
-                System.err.println(cf.channel().localAddress() + ": ok");
-            })
-            .whenCancel(cf -> System.err.println("cancel"))
-            .whenFailure(cf -> {
-                System.err.println(cf.channel().localAddress() + ": fail, " + cf.cause());
-                cf.channel().eventLoop().schedule(() -> testMultiTcp.connect(channelKey(cf)), 2, TimeUnit.SECONDS);
-            })
-            .whenDone(cf -> System.err.println("done"));
+                    System.err.println(cf.channel().localAddress() + ": ok");
+                })
+                .whenCancel(cf -> System.err.println("cancel"))
+                .whenFailure(cf -> {
+                    System.err.println(cf.channel().localAddress() + ": fail, " + cf.cause());
+                    cf.channel().eventLoop().schedule(() -> testMultiTcp.connect(channelKey(cf)), 2, TimeUnit.SECONDS);
+                })
+                .whenDone(cf -> System.err.println("done"));
 
         testMultiTcp.connectAll().values().forEach(c -> c.addListener(listener));
 
--- src/test/java/server/TestServer.java
+++ src/test/java/server/TestServer.java
@@ -1,21 +1,23 @@
 package server;
 
-import static io.netty.buffer.Unpooled.wrappedBuffer;
-
 import codec.UserCodec;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
-import java.net.SocketAddress;
-import java.util.Arrays;
 import org.fz.nettyx.codec.EscapeCodec;
 import org.fz.nettyx.codec.EscapeCodec.EscapeMap;
 import org.fz.nettyx.codec.StartEndFlagFrameCodec;
 import org.fz.nettyx.endpoint.server.TcpServer;
 import org.fz.nettyx.handler.ChannelAdvice.InboundAdvice;
 import org.fz.nettyx.handler.ChannelAdvice.OutboundAdvice;
+
+import java.net.SocketAddress;
+import java.util.Arrays;
+
+import static io.netty.buffer.Unpooled.wrappedBuffer;
 
 /**
  * @author fengbinbin
@@ -39,34 +41,35 @@ public class TestServer extends TcpServer {
             @Override
             protected void initChannel(SocketChannel channel) {
                 InboundAdvice inboundAdvice = new InboundAdvice(channel)
-                    .whenExceptionCaught((c, t) -> System.err.println("in error: [" + t + "]"));
+                        .whenExceptionCaught((c, t) -> System.err.println("in error: [" + t + "]"));
                 OutboundAdvice outboundAdvice = new OutboundAdvice(channel)
-                    .whenExceptionCaught((c, t) -> System.err.println("out error: [" + t + "]"));
+                        .whenExceptionCaught((c, t) -> System.err.println("out error: [" + t + "]"));
 
                 channel.pipeline().addLast(
-                    outboundAdvice
-                    , new StartEndFlagFrameCodec(320, true, wrappedBuffer(new byte[]{(byte) 0x7e}))
-                    , new EscapeCodec(EscapeMap.mapHex("7e", "7d5e"))
-                    ,new ChannelInboundHandlerAdapter(){
-                        @Override
-                        public void channelRead(ChannelHandlerContext ctx, Object in) throws Exception {
-                            byte[] msg = new byte[300];
-                            Arrays.fill(msg, (byte) 1);
-                            ctx.channel().writeAndFlush(Unpooled.wrappedBuffer(msg));
-                            super.channelRead(ctx, in);
+                        outboundAdvice
+                        , new StartEndFlagFrameCodec(320, true, wrappedBuffer(new byte[]{(byte) 0x7e}))
+                        , new EscapeCodec(EscapeMap.mapHex("7e", "7d5e"))
+                        , new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object in) throws Exception {
+                                byte[] msg = new byte[300];
+                                Arrays.fill(msg, (byte) 1);
+                                ctx.channel().writeAndFlush(Unpooled.wrappedBuffer(msg));
+                                super.channelRead(ctx, in);
+                            }
                         }
-                    }
-                    , new UserCodec()
+                        , new UserCodec()
 
-                    , inboundAdvice);
+                        , inboundAdvice);
             }
         };
     }
 
     public static void main(String[] args) {
-        TestServer testServer = new TestServer(9888);
-
-        testServer.bind().channel().closeFuture().addListener(cf -> {
+        TestServer    testServer = new TestServer(9888);
+        ChannelFuture bindFuture = testServer.bind();
+        bindFuture.addListener(cf -> System.err.println("binding state:" + cf.isSuccess()));
+        bindFuture.channel().closeFuture().addListener(cf -> {
             System.err.println("关闭了");
             testServer.shutdownGracefully();
         });
