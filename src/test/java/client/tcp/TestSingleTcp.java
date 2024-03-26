d6122f5a41d3167204d005bc0589472bd0c981c5
fengbinbin <290361638@qq.com>
2024-03-26 09:32:29 +0800

    test(test): add spring test

--- src/test/java/client/tcp/TestSingleTcp.java
+++ src/test/java/client/tcp/TestSingleTcp.java
@@ -6,14 +6,15 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+import org.fz.nettyx.endpoint.client.tcp.SingleTcpChannelClient;
+import org.fz.nettyx.listener.ActionChannelFutureListener;
+
 import java.net.InetSocketAddress;
 import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import lombok.extern.slf4j.Slf4j;
-import org.fz.nettyx.endpoint.client.tcp.SingleTcpChannelClient;
-import org.fz.nettyx.listener.ActionChannelFutureListener;
 
 @Slf4j
 public class TestSingleTcp extends SingleTcpChannelClient {
@@ -30,24 +31,24 @@ public class TestSingleTcp extends SingleTcpChannelClient {
     }
 
     public static void main(String[] args) {
-        TestSingleTcp testClient = new TestSingleTcp(new InetSocketAddress("127.0.0.1", 9888));
+        TestSingleTcp testClient = new TestSingleTcp(new InetSocketAddress(9888));
 
         ChannelFutureListener listener = new ActionChannelFutureListener()
-            .whenSuccess(cf -> {
-                executor.scheduleAtFixedRate(() -> {
-                    byte[] msg = new byte[300];
-                    Arrays.fill(msg, (byte) 1);
-                    testClient.writeAndFlush(Unpooled.wrappedBuffer(msg));
-                }, 2, 30, TimeUnit.MILLISECONDS);
+                .whenSuccess(cf -> {
+                    executor.scheduleAtFixedRate(() -> {
+                        byte[] msg = new byte[300];
+                        Arrays.fill(msg, (byte) 1);
+                        testClient.writeAndFlush(Unpooled.wrappedBuffer(msg));
+                    }, 2, 30, TimeUnit.MILLISECONDS);
 
-                System.err.println(cf.channel().localAddress() + ": ok");
-            })
-            .whenCancel(cf -> System.err.println("cancel"))
-            .whenFailure(cf -> {
-                System.err.println(cf.channel().localAddress() + ": fail, " + cf.cause());
-                cf.channel().eventLoop().schedule(testClient::connect, 2, TimeUnit.SECONDS);
-            })
-            .whenDone(cf -> System.err.println("done"));
+                    System.err.println(cf.channel().localAddress() + ": ok");
+                })
+                .whenCancel(cf -> System.err.println("cancel"))
+                .whenFailure(cf -> {
+                    System.err.println(cf.channel().localAddress() + ": fail, " + cf.cause());
+                    cf.channel().eventLoop().schedule(testClient::connect, 2, TimeUnit.SECONDS);
+                })
+                .whenDone(cf -> System.err.println("done"));
 
         testClient.connect().addListener(listener);
     }
