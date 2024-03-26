63696c9f3d35819d342c3924c1cf32777056e96c
fengbinbin <290361638@qq.com>
2024-03-26 14:04:06 +0800

    test(test): add spring test

--- src/test/java/client/rxtx/TestMultiRxtx.java
+++ src/test/java/client/rxtx/TestMultiRxtx.java
@@ -1,13 +1,23 @@
 package client.rxtx;
 
 import client.TestChannelInitializer;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import org.fz.nettyx.endpoint.client.rxtx.MultiRxtxChannelClient;
 import org.fz.nettyx.endpoint.client.rxtx.support.XRxtxChannel;
 import org.fz.nettyx.endpoint.client.rxtx.support.XRxtxDeviceAddress;
+import org.fz.nettyx.listener.ActionChannelFutureListener;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.fz.nettyx.listener.ActionChannelFutureListener.redo;
 
 /**
  * @author fengbinbin
@@ -15,6 +25,8 @@ import java.util.Map;
  * @since 2024/3/1 22:58
  */
 public class TestMultiRxtx extends MultiRxtxChannelClient<String> {
+
+    static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
     protected TestMultiRxtx(Map<String, XRxtxDeviceAddress> stringRxtxDeviceAddressMap) {
         super(stringRxtxDeviceAddressMap);
@@ -27,7 +39,21 @@ public class TestMultiRxtx extends MultiRxtxChannelClient<String> {
         map.put("6", new XRxtxDeviceAddress("COM6"));
 
         TestMultiRxtx testMultiTcp = new TestMultiRxtx(map);
-        testMultiTcp.connectAll();
+        ChannelFutureListener listener = new ActionChannelFutureListener()
+                .whenSuccess((l, cf) -> {
+                    executor.scheduleAtFixedRate(() -> {
+                        byte[] msg = new byte[300];
+                        Arrays.fill(msg, (byte) 1);
+                        cf.channel().writeAndFlush(Unpooled.wrappedBuffer(msg));
+                    }, 2, 30, TimeUnit.MILLISECONDS);
+
+                    System.err.println(cf.channel().localAddress() + ": ok");
+                })
+                .whenCancel((l, cf) -> System.err.println("cancel"))
+                .whenFailure(redo(cf -> testMultiTcp.connect(channelKey(cf)), 2, SECONDS))
+                .whenDone((l, cf) -> System.err.println("done"));
+
+        testMultiTcp.connectAll().values().forEach(c -> c.addListener(listener));
     }
 
     @Override
