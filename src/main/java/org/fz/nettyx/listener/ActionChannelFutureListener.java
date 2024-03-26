25f92f7c10667eeebac1e5c08fbeaec0db101aa4
fengbinbin <290361638@qq.com>
2024-03-26 09:45:41 +0800

    test(test): add spring test

--- src/main/java/org/fz/nettyx/listener/ActionChannelFutureListener.java
+++ src/main/java/org/fz/nettyx/listener/ActionChannelFutureListener.java
@@ -1,13 +1,10 @@
 package org.fz.nettyx.listener;
 
-import static org.fz.nettyx.action.Actions.invokeAction;
-
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
-import org.fz.nettyx.action.ChannelFutureAction;
 
 /**
  * The type Actionable channel future listener.
@@ -24,25 +21,36 @@ public class ActionChannelFutureListener implements ChannelFutureListener {
     /**
      * When the different state of the channel is monitored, the corresponding method will be called
      */
-    private ChannelFutureAction
-        whenSuccess,
-        whenFailure,
-        whenCancel,
-        whenDone;
+    private ListenerAction
+            whenSuccess,
+            whenFailure,
+            whenCancel,
+            whenDone;
 
     @Override
     public void operationComplete(ChannelFuture channelFuture) {
         // success
-        if (channelFuture.isSuccess())   invokeAction(whenSuccess, channelFuture);
+        if (channelFuture.isSuccess()) invokeAction(whenSuccess, this, channelFuture);
         else
         // failed
-        if (!channelFuture.isSuccess())  invokeAction(whenFailure, channelFuture);
+        if (!channelFuture.isSuccess()) invokeAction(whenFailure, this, channelFuture);
 
         // done
-        if (channelFuture.isDone())      invokeAction(whenDone,    channelFuture);
+        if (channelFuture.isDone()) invokeAction(whenDone, this, channelFuture);
         else
         // canceled
-        if (channelFuture.isCancelled()) invokeAction(whenCancel,  channelFuture);
+        if (channelFuture.isCancelled()) invokeAction(whenCancel, this, channelFuture);
+    }
+
+
+    public interface ListenerAction {
+        void act(ChannelFutureListener listener, ChannelFuture channelFuture);
+    }
+
+    public static void invokeAction(ListenerAction action, ChannelFutureListener listener, ChannelFuture cf) {
+        if (action != null) {
+            action.act(listener, cf);
+        }
     }
 
 }
