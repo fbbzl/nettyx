b3d935a27f25cc73b5b0ad1f7d91b16a8abe9a37
fengbinbin <290361638@qq.com>
2024-03-27 15:40:35 +0800

    feat(upgrade): 2.2.12-RELEASE

--- src/main/java/org/fz/nettyx/endpoint/client/rxtx/support/RxtxChannel.java
+++ src/main/java/org/fz/nettyx/endpoint/client/rxtx/support/RxtxChannel.java
@@ -3,12 +3,9 @@ package org.fz.nettyx.endpoint.client.rxtx.support;
 import gnu.io.CommPort;
 import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelPromise;
 import org.fz.nettyx.channel.SerialCommPortChannel;
 
 import java.net.SocketAddress;
-import java.util.concurrent.TimeUnit;
 
 import static org.fz.nettyx.endpoint.client.rxtx.support.RxtxChannelOption.*;
 
@@ -25,7 +22,6 @@ public class RxtxChannel extends SerialCommPortChannel {
 
     private final RxtxChannelConfig config;
 
-    private boolean           open = true;
     private RxtxDeviceAddress deviceAddress;
     private SerialPort        serialPort;
 
@@ -39,16 +35,6 @@ public class RxtxChannel extends SerialCommPortChannel {
     }
 
     @Override
-    public boolean isOpen() {
-        return open;
-    }
-
-    @Override
-    protected AbstractUnsafe newUnsafe() {
-        return new RxtxUnsafe();
-    }
-
-    @Override
     protected void doConnect(SocketAddress remoteAddress, SocketAddress localAddress) throws Exception {
         RxtxDeviceAddress        remote   = (RxtxDeviceAddress) remoteAddress;
         final CommPortIdentifier cpi      = CommPortIdentifier.getPortIdentifier(remote.value());
@@ -59,7 +45,7 @@ public class RxtxChannel extends SerialCommPortChannel {
         serialPort = (SerialPort) commPort;
     }
 
-
+    @Override
     protected void doInit() throws Exception {
         serialPort.setSerialPortParams(
                 config().getOption(BAUD_RATE),
@@ -94,67 +80,8 @@ public class RxtxChannel extends SerialCommPortChannel {
     }
 
     @Override
-    protected void doBind(SocketAddress localAddress) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    protected void doDisconnect() throws Exception {
-        doClose();
-    }
-
-    @Override
     protected boolean isInputShutdown() {
         return !open;
-    }
-
-    @Override
-    protected ChannelFuture shutdownInput() {
-        return newFailedFuture(new UnsupportedOperationException("shutdownInput"));
-    }
-
-    private final class RxtxUnsafe extends AbstractUnsafe {
-        @Override
-        public void connect(
-                final SocketAddress remoteAddress,
-                final SocketAddress localAddress, final ChannelPromise promise) {
-            if (!promise.setUncancellable() || !ensureOpen(promise)) {
-                return;
-            }
-
-            try {
-                final boolean wasActive = isActive();
-                doConnect(remoteAddress, localAddress);
-
-                int waitTime = config().getOption(WAIT_TIME);
-                if (waitTime > 0) {
-                    eventLoop().schedule(() -> {
-                        try {
-                            doInit();
-                            safeSetSuccess(promise);
-                            if (!wasActive && isActive()) {
-                                pipeline().fireChannelActive();
-                            }
-                        }
-                        catch (Throwable t) {
-                            safeSetFailure(promise, t);
-                            closeIfClosed();
-                        }
-                    }, waitTime, TimeUnit.MILLISECONDS);
-                }
-                else {
-                    doInit();
-                    safeSetSuccess(promise);
-                    if (!wasActive && isActive()) {
-                        pipeline().fireChannelActive();
-                    }
-                }
-            }
-            catch (Throwable t) {
-                safeSetFailure(promise, t);
-                closeIfClosed();
-            }
-        }
     }
 
     @Override
