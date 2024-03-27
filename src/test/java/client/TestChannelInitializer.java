790cc48576a8640e6abcdf8164b8af0ea86ebd46
fengbinbin <290361638@qq.com>
2024-03-27 10:39:24 +0800

    feat(upgrade): 2.2.12-RELEASE

--- src/test/java/client/TestChannelInitializer.java
+++ src/test/java/client/TestChannelInitializer.java
@@ -3,8 +3,8 @@ package client;
 import codec.UserCodec;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.handler.ssl.OptionalSslHandler;
 import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.fz.nettyx.codec.EscapeCodec;
 import org.fz.nettyx.codec.EscapeCodec.EscapeMap;
@@ -29,20 +29,17 @@ public class TestChannelInitializer<C extends Channel> extends ChannelInitialize
 
     @Override
     protected void initChannel(C channel) throws SSLException {
-        // 建议使用 spring的配置bean, 这里为了演示, 直接new
-        OpenSslConfig clientSslConfig = new OpenSslConfig();
-        clientSslConfig.setCert("/usr/local/yourapp/ssl/client/xxx.cer");
-        clientSslConfig.setKey("/usr/local/yourapp/ssl/client/xxx.key");
-        clientSslConfig.setKeyPass("aasdf2@##$");
-        clientSslConfig.setRoot("/usr/local/yourapp/ssl/root/yyy.cer");
+        OpenSslConfig serverSslConfig = new OpenSslConfig();
+        serverSslConfig.setCert("/usr/local/yourapp/ssl/server/xxx.cer");
+        serverSslConfig.setKey("/usr/local/yourapp/ssl/server/xxx.key");
+        serverSslConfig.setKeyPass("aasdf2@##$");
+        serverSslConfig.setRoot("/usr/local/yourapp/ssl/root/yyy.cer");
 
-        // 尽量复用SslContext实例, 此处获取的是客户端的sslcontext
-        SslContext clientSslContext = new OpenSslContextFactory(clientSslConfig).getClientSslContext();
-        SslHandler clientSslHandler       = clientSslContext.newHandler(channel.alloc());
+        SslContext serverSslContext = new OpenSslContextFactory(serverSslConfig).getServerSslContext();
 
         channel.pipeline().addLast(
                 // 如果程序 既要接收ssl又要接收非ssl, 可以使用OptionalSslHandler
-                clientSslHandler
+                new OptionalSslHandler(serverSslContext)
                 , new StartEndFlagFrameCodec(320, true, wrappedBuffer(new byte[]{(byte) 0x7e}))
                 , new EscapeCodec(EscapeMap.mapHex("7e", "7d5e"))
                 , new UserCodec()
