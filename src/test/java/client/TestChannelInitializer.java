225b26a816c868c3d49b0ca69f67846235476e13
fengbinbin <290361638@qq.com>
2024-03-27 16:33:27 +0800

    feat(upgrade): 2.2.12-RELEASE

--- src/test/java/client/TestChannelInitializer.java
+++ src/test/java/client/TestChannelInitializer.java
@@ -3,15 +3,11 @@ package client;
 import codec.UserCodec;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
-import io.netty.handler.ssl.OptionalSslHandler;
-import io.netty.handler.ssl.SslContext;
 import lombok.extern.slf4j.Slf4j;
 import org.fz.nettyx.codec.EscapeCodec;
 import org.fz.nettyx.codec.EscapeCodec.EscapeMap;
 import org.fz.nettyx.codec.StartEndFlagFrameCodec;
 import org.fz.nettyx.handler.LoggerHandler;
-import org.fz.nettyx.ssl.OpenSslContextFactory;
-import org.fz.nettyx.ssl.OpenSslContextFactory.OpenSslConfig;
 
 import javax.net.ssl.SSLException;
 
@@ -29,17 +25,8 @@ public class TestChannelInitializer<C extends Channel> extends ChannelInitialize
 
     @Override
     protected void initChannel(C channel) throws SSLException {
-        OpenSslConfig serverSslConfig = new OpenSslConfig();
-        serverSslConfig.setCert("/usr/local/yourapp/ssl/server/xxx.cer");
-        serverSslConfig.setKey("/usr/local/yourapp/ssl/server/xxx.key");
-        serverSslConfig.setKeyPass("aasdf2@##$");
-        serverSslConfig.setRoot("/usr/local/yourapp/ssl/root/yyy.cer");
-
-        SslContext serverSslContext = new OpenSslContextFactory(serverSslConfig).getServerSslContext();
-
         channel.pipeline().addLast(
-                new OptionalSslHandler(serverSslContext)
-                , new StartEndFlagFrameCodec(320, true, wrappedBuffer(new byte[]{(byte) 0x7e}))
+                new StartEndFlagFrameCodec(320, true, wrappedBuffer(new byte[]{(byte) 0x7e}))
                 , new EscapeCodec(EscapeMap.mapHex("7e", "7d5e"))
                 , new UserCodec()
                 , new LoggerHandler(log, INFO));
