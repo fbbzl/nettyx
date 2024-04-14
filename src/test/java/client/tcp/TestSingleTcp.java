89a02f6bc131592fbcc5d8002d5165e9b359c4cf
fengbinbin <290361638@qq.com>
2024-04-14 21:53:33 +0800

    feat(upgrade): update 2.2.18

--- src/test/java/client/tcp/TestSingleTcp.java
+++ src/test/java/client/tcp/TestSingleTcp.java
@@ -49,7 +49,7 @@ public class TestSingleTcp extends SingleTcpChannelClient {
                 .whenFailure(redo(() -> {
                     log.error("redo");
                     return testClient.connect();
-                }, 2))
+                }, 3))
                 .whenDone((ls, cf) -> System.err.println("done"));
 
         testClient.connect().addListener(listener);
