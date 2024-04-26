ddfe5254233bf54be81f2b88ccc3b8033a68fd65
fengbinbin <290361638@qq.com>
2024-04-26 10:50:31 +0800

    feat(upgrade): update 2.2.21

--- src/main/java/org/fz/nettyx/endpoint/client/jsc/support/JscChannelOption.java
+++ src/main/java/org/fz/nettyx/endpoint/client/jsc/support/JscChannelOption.java
@@ -2,9 +2,9 @@ package org.fz.nettyx.endpoint.client.jsc.support;
 
 
 import io.netty.channel.ChannelOption;
+import org.fz.nettyx.endpoint.client.jsc.support.JscChannelConfig.DataBits;
 import org.fz.nettyx.endpoint.client.jsc.support.JscChannelConfig.ParityBit;
 import org.fz.nettyx.endpoint.client.jsc.support.JscChannelConfig.StopBits;
-
 
 /**
  * Option for configuring a serial port connection.
@@ -20,7 +20,7 @@ public final class JscChannelOption<T> extends ChannelOption<T> {
     public static final ChannelOption<Boolean>   DTR          = valueOf(JscChannelOption.class, "DTR");
     public static final ChannelOption<Boolean>   RTS          = valueOf(JscChannelOption.class, "RTS");
     public static final ChannelOption<StopBits>  STOP_BITS    = valueOf(JscChannelOption.class, "STOP_BITS");
-    public static final ChannelOption<Integer>   DATA_BITS    = valueOf(JscChannelOption.class, "DATA_BITS");
+    public static final ChannelOption<DataBits>  DATA_BITS    = valueOf(JscChannelOption.class, "DATA_BITS");
     public static final ChannelOption<ParityBit> PARITY_BIT   = valueOf(JscChannelOption.class, "PARITY_BIT");
     public static final ChannelOption<Integer>   WAIT_TIME    = valueOf(JscChannelOption.class, "WAIT_TIME");
     public static final ChannelOption<Integer>   READ_TIMEOUT = valueOf(JscChannelOption.class, "READ_TIMEOUT");
