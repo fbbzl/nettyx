213a69a6941f76ea0115096f89716d8ffcc93c4c
fengbinbin <290361638@qq.com>
2024-05-15 17:23:22 +0800

    feat(upgrade): update 2.3.1

--- src/main/java/org/fz/nettyx/endpoint/bluetooth/client/SingleBluetoothChannelClient.java
+++ src/main/java/org/fz/nettyx/endpoint/bluetooth/client/SingleBluetoothChannelClient.java
@@ -2,7 +2,6 @@ package org.fz.nettyx.endpoint.bluetooth.client;
 
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.oio.OioEventLoopGroup;
-import org.fz.nettyx.channel.SerialCommChannel;
 import org.fz.nettyx.channel.bluetooth.BluetoothChannel;
 import org.fz.nettyx.channel.bluetooth.BluetoothChannelConfig;
 import org.fz.nettyx.endpoint.AbstractSingleChannellEndpoint;
@@ -16,7 +15,7 @@ import org.fz.nettyx.endpoint.AbstractSingleChannellEndpoint;
 public abstract class SingleBluetoothChannelClient extends AbstractSingleChannellEndpoint<BluetoothChannel, BluetoothChannelConfig> {
 
     protected SingleBluetoothChannelClient(String commAddress) {
-        super(new SerialCommChannel.SerialCommAddress(commAddress));
+        super(new BluetoothChannel.BluetoothDeviceAddress(commAddress));
     }
 
     protected SingleBluetoothChannelClient(BluetoothChannel.BluetoothDeviceAddress address) {
