497cc19e8653673754f7252bd6ae54a67a4a1fe5
fengbinbin <290361638@qq.com>
2024-05-15 19:17:55 +0800

    feat(upgrade): update 2.3.1

--- src/test/java/client/bluetooth/TestBluetooth.java
+++ src/test/java/client/bluetooth/TestBluetooth.java
@@ -1,7 +1,12 @@
 package client.bluetooth;
 
-import javax.bluetooth.BluetoothStateException;
-import javax.bluetooth.LocalDevice;
+import javax.bluetooth.*;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static javax.bluetooth.ServiceRecord.NOAUTHENTICATE_NOENCRYPT;
 
 /**
  * @author fengbinbin
@@ -10,9 +15,53 @@ import javax.bluetooth.LocalDevice;
  */
 public class TestBluetooth {
 
-
-    public static void main(String[] args) throws BluetoothStateException {
+    public static void main(String[] args) throws Exception {
         LocalDevice localDevice = LocalDevice.getLocalDevice();
-        System.err.println(localDevice);
+        System.out.println("本地蓝牙适配器名称：" + localDevice.getFriendlyName());
+        System.out.println("本地蓝牙适配器地址：" + localDevice.getBluetoothAddress());
+
+        DiscoveryAgent agent = localDevice.getDiscoveryAgent();
+        agent.startInquiry(DiscoveryAgent.GIAC, new DiscoveryListener() {
+            @Override
+            public void deviceDiscovered(RemoteDevice remoteDevice, DeviceClass deviceClass) {
+
+            }
+
+            @Override
+            public void servicesDiscovered(int i, ServiceRecord[] serviceRecords) {
+
+            }
+
+            @Override
+            public void serviceSearchCompleted(int i, int i1) {
+
+            }
+
+            @Override
+            public void inquiryCompleted(int i) {
+
+            }
+        });
+
+
+        RemoteDevice remoteDevice = agent.retrieveDevices(DiscoveryAgent.PREKNOWN)[0];
+        UUID         uuid         = new UUID(NOAUTHENTICATE_NOENCRYPT);
+
+        String           url              = agent.selectService(uuid, NOAUTHENTICATE_NOENCRYPT, false);
+        StreamConnection streamConnection = (StreamConnection) Connector.open(url);
+
+        // 发送数据
+        OutputStream os = streamConnection.openOutputStream();
+        os.write("Hello, Bluetooth!".getBytes());
+
+        // 接收数据
+        InputStream is     = streamConnection.openInputStream();
+        byte[]      buffer = new byte[1024];
+        int         bytes;
+        while ((bytes = is.read(buffer)) != -1) {
+            String receivedData = new String(buffer, 0, bytes);
+            System.out.println("接收到数据：" + receivedData);
+        }
+
     }
 }
