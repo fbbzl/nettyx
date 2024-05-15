90c68c807f8690dcafc0fd7cbfb2ded608ee347e
fengbinbin <290361638@qq.com>
2024-05-15 17:12:02 +0800

    feat(upgrade): update 2.3.1

--- src/main/java/org/fz/nettyx/endpoint/bluetooth/client/SingleBluetoothChannelClient.java
+++ src/main/java/org/fz/nettyx/endpoint/bluetooth/client/SingleBluetoothChannelClient.java
@@ -1,9 +1,30 @@
 package org.fz.nettyx.endpoint.bluetooth.client;
 
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.oio.OioEventLoopGroup;
+import org.fz.nettyx.channel.SerialCommChannel;
+import org.fz.nettyx.channel.bluetooth.BluetoothChannel;
+import org.fz.nettyx.channel.bluetooth.BluetoothChannelConfig;
+import org.fz.nettyx.endpoint.AbstractSingleChannellEndpoint;
+
 /**
  * @author fengbinbin
  * @version 1.0
  * @since 2024/5/15 12:21
  */
-public class SingleBluetoothChannelClient {
+@SuppressWarnings("deprecation")
+public abstract class SingleBluetoothChannelClient extends AbstractSingleChannellEndpoint<BluetoothChannel, BluetoothChannelConfig> {
+
+    protected SingleBluetoothChannelClient(String commAddress) {
+        super(new SerialCommChannel.SerialCommAddress(commAddress));
+    }
+
+    protected SingleBluetoothChannelClient(SerialCommChannel.SerialCommAddress commAddress) {
+        super(commAddress);
+    }
+
+    @Override
+    protected EventLoopGroup newEventLoopGroup() {
+        return new OioEventLoopGroup();
+    }
 }
