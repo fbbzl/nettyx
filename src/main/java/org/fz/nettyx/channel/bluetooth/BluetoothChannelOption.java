f2911e82806ef3891c20fec0685eddd57b4ca43e
fengbinbin <290361638@qq.com>
2024-05-16 23:34:53 +0800

    feat(upgrade): update 2.3.1

--- src/main/java/org/fz/nettyx/channel/bluetooth/BluetoothChannelOption.java
+++ src/main/java/org/fz/nettyx/channel/bluetooth/BluetoothChannelOption.java
@@ -1,12 +1,23 @@
 package org.fz.nettyx.channel.bluetooth;
 
+import io.netty.channel.ChannelOption;
+
 /**
  * @author fengbinbin
  * @version 1.0
  * @since 2024/5/15 15:18
  */
-public class BluetoothChannelOption {
+public class BluetoothChannelOption<T> extends ChannelOption<T> {
 
 
 
+
+    private static <O> ChannelOption<O> option(String secondNameComponent) {
+        return valueOf(BluetoothChannelOption.class, secondNameComponent);
+    }
+
+    @SuppressWarnings("deprecation")
+    private BluetoothChannelOption() {
+        super("bluetooth-config");
+    }
 }
