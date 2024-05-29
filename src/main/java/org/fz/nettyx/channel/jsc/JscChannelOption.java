b88fc1c298bf0a92f02e8536042b1fa843efc354
fengbinbin <290361638@qq.com>
2024-05-29 22:58:09 +0800

    feat(upgrade): update 2.3.1

--- src/main/java/org/fz/nettyx/channel/jsc/JscChannelOption.java
+++ src/main/java/org/fz/nettyx/channel/jsc/JscChannelOption.java
@@ -19,7 +19,6 @@ public final class JscChannelOption<T> extends ChannelOption<T> {
     public static final ChannelOption<JscChannelConfig.StopBits>  STOP_BITS    = option("STOP_BITS");
     public static final ChannelOption<JscChannelConfig.DataBits>  DATA_BITS    = option("DATA_BITS");
     public static final ChannelOption<JscChannelConfig.ParityBit> PARITY_BIT   = option("PARITY_BIT");
-    public static final ChannelOption<Integer>                    WAIT_TIME    = option("WAIT_TIME");
     public static final ChannelOption<Integer>                    READ_TIMEOUT = option("READ_TIMEOUT");
 
     private static <O> ChannelOption<O> option(String secondNameComponent) {
