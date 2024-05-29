86d3e90e465dd62320b321e7cda3dd99b93670a0
fengbinbin <290361638@qq.com>
2024-05-29 23:27:09 +0800

    feat(upgrade): update 2.3.1

--- src/main/java/org/fz/nettyx/channel/rxtx/RxtxChannel.java
+++ src/main/java/org/fz/nettyx/channel/rxtx/RxtxChannel.java
@@ -2,10 +2,8 @@ package org.fz.nettyx.channel.rxtx;
 
 import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
-import gnu.io.UnsupportedCommOperationException;
 import org.fz.nettyx.channel.SerialCommChannel;
 
-import java.io.IOException;
 import java.net.SocketAddress;
 
 
@@ -35,22 +33,16 @@ public class RxtxChannel extends SerialCommChannel {
         this.serialPort    = (SerialPort) CommPortIdentifier.getPortIdentifier(this.remoteAddress.value()).open(getClass().getName(), 1000);
         this.serialPort.enableReceiveTimeout(config().getOption(RxtxChannelOption.READ_TIMEOUT));
 
-        try {
-            serialPort.setSerialPortParams(
-                    config().getOption(RxtxChannelOption.BAUD_RATE),
-                    config().getOption(RxtxChannelOption.DATA_BITS).value(),
-                    config().getOption(RxtxChannelOption.STOP_BITS).value(),
-                    config().getOption(RxtxChannelOption.PARITY_BIT).value()
-                                          );
-            serialPort.setDTR(config().getOption(RxtxChannelOption.DTR));
-            serialPort.setRTS(config().getOption(RxtxChannelOption.RTS));
+        serialPort.setSerialPortParams(
+                config().getOption(RxtxChannelOption.BAUD_RATE),
+                config().getOption(RxtxChannelOption.DATA_BITS).value(),
+                config().getOption(RxtxChannelOption.STOP_BITS).value(),
+                config().getOption(RxtxChannelOption.PARITY_BIT).value()
+                                      );
+        serialPort.setDTR(config().getOption(RxtxChannelOption.DTR));
+        serialPort.setRTS(config().getOption(RxtxChannelOption.RTS));
 
-            super.activate(serialPort.getInputStream(), serialPort.getOutputStream());
-        } catch (UnsupportedCommOperationException commPortError) {
-            throw new IllegalArgumentException("can not open comm port", commPortError);
-        } catch (IOException steamError) {
-            throw new UnsupportedOperationException("can get input/output stream, please check", steamError);
-        }
+        super.activate(serialPort.getInputStream(), serialPort.getOutputStream());
 
     }
 
