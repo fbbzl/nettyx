bf5497ffcd210e063e3bd74eecb8def725f71760
fengbinbin <290361638@qq.com>
2024-05-30 20:08:09 +0800

    feat(upgrade): update 2.3.1

--- src/test/java/template/bluetooth/BluetoothClientTest.java
+++ src/test/java/template/bluetooth/BluetoothClientTest.java
@@ -1,11 +1,25 @@
 package template.bluetooth;
 
+import io.netty.channel.ChannelInitializer;
+import org.fz.nettyx.channel.bluetooth.client.BluetoothChannel;
+import org.fz.nettyx.template.bluetooth.client.SingleBluetoothChannelClient;
+import template.TestChannelInitializer;
+
 /**
  * @author fengbinbin
  * @version 1.0
  * @since 2024/5/30 19:57
  */
-public class BluetoothClientTest {
+public class BluetoothClientTest extends SingleBluetoothChannelClient {
+    
+    public BluetoothClientTest(String commAddress) {
+        super(commAddress);
+    }
+
+    @Override
+    protected ChannelInitializer<BluetoothChannel> channelInitializer() {
+        return new TestChannelInitializer<>();
+    }
 
     public static void main(String[] args) {
 
