b5ae10d156f5ede39ca5be7bc1aa0d92acf67ec4
fengbinbin <290361638@qq.com>
2024-05-30 21:13:28 +0800

    feat(upgrade): update 2.3.1

---
+++ src/test/java/template/bluetooth/TestBtClient.java
@@ -0,0 +1,42 @@
+package template.bluetooth;
+
+import cn.hutool.core.util.StrUtil;
+import io.netty.channel.ChannelInitializer;
+import org.fz.nettyx.channel.bluetooth.client.BtChannel;
+import org.fz.nettyx.template.bluetooth.client.SingleBtChannelTemplate;
+import org.fz.nettyx.util.BluetoothFinder;
+import template.DebugChannelListener;
+import template.TestChannelInitializer;
+
+import javax.bluetooth.RemoteDevice;
+import java.util.List;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/5/30 19:57
+ */
+public class TestBtClient extends SingleBtChannelTemplate {
+
+    public TestBtClient(String address) {
+        super(address);
+    }
+
+    @Override
+    protected ChannelInitializer<BtChannel> channelInitializer() {
+        return new TestChannelInitializer<>();
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<RemoteDevice> devices = new BluetoothFinder.DeviceFinder().getDevices();
+        for (RemoteDevice device : devices) {
+            String       url = StrUtil.format("btspp://{}:1;authenticate=false;encrypt=false;master=false", device.getBluetoothAddress());
+
+            TestBtClient client = new TestBtClient(url);
+
+            client.connect().addListener(new DebugChannelListener());
+        }
+
+    }
+
+}
