622c8566823f9124eba580f5e96cb5481af36a94
fengbinbin <290361638@qq.com>
2024-05-30 21:30:14 +0800

    feat(upgrade): update 2.3.1

---
+++ src/test/java/template/bluetooth/demo/BluetoothChannelTest.java
@@ -0,0 +1,48 @@
+package template.bluetooth.demo;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/5/30 21:20
+ */
+
+import cn.hutool.core.util.StrUtil;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import org.fz.nettyx.util.BtFinder;
+import org.junit.Test;
+
+import javax.bluetooth.RemoteDevice;
+import java.util.List;
+
+public class BluetoothChannelTest {
+
+    @Test
+    public void testConnect() {
+        Bootstrap      bootstrap   = new Bootstrap();
+        EventLoopGroup workerGroup = new OioEventLoopGroup();
+
+        List<RemoteDevice> devices = new BtFinder.DeviceFinder().getDevices();
+        for (RemoteDevice device : devices) {
+            String       url = StrUtil.format("btspp://{}:1;authenticate=false;encrypt=false;master=false", device.getBluetoothAddress());
+            bootstrap.group(workerGroup).channel(OioBluetoothChannel.class).remoteAddress(new BluetoothDeviceAddress(url))
+                     .handler(new ChannelInitializer<OioBluetoothChannel>() {
+
+                         @Override
+                         public void initChannel(OioBluetoothChannel ch) {
+                             ch.pipeline().addLast(new StringDecoder()).addLast(new StringEncoder());
+                         }
+                     });
+
+            bootstrap.connect().syncUninterruptibly();
+
+
+
+        }
+
+    }
+}
