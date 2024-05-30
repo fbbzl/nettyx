98097045f802208fe01e992a58a4e2528287062a
fengbinbin <290361638@qq.com>
2024-05-30 23:55:04 +0800

    feat(upgrade): update 2.3.1

--- src/test/java/template/bluetooth/TestBtClient.java
+++ src/test/java/template/bluetooth/TestBtClient.java
@@ -1,15 +1,23 @@
 package template.bluetooth;
 
+import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.StrUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import org.fz.nettyx.channel.bluetooth.client.BtChannel;
 import org.fz.nettyx.channel.bluetooth.finder.BtFinder;
+import org.fz.nettyx.channel.jsc.JscChannelConfig;
+import org.fz.nettyx.listener.ActionChannelFutureListener;
 import org.fz.nettyx.template.bluetooth.client.SingleBtChannelTemplate;
-import template.DebugChannelListener;
 import template.TestChannelInitializer;
 
 import javax.bluetooth.RemoteDevice;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author fengbinbin
@@ -17,7 +25,7 @@ import java.util.List;
  * @since 2024/5/30 19:57
  */
 public class TestBtClient extends SingleBtChannelTemplate {
-
+    static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
     public TestBtClient(String address) {
         super(address);
     }
@@ -30,13 +38,31 @@ public class TestBtClient extends SingleBtChannelTemplate {
     public static void main(String[] args) throws Exception {
         List<RemoteDevice> devices = new BtFinder.DeviceFinder().getDevices();
 
-        RemoteDevice device = devices.get(0);
+        for (RemoteDevice device : devices) {
+            System.err.println(device.getBluetoothAddress());
+            System.err.println(device.getFriendlyName(false));
+        }
 
-        String       url = StrUtil.format("btspp://{}:1;authenticate=false;encrypt=false;master=false", device.getBluetoothAddress());
+        String       url = StrUtil.format("btspp://{}:1;authenticate=false;encrypt=false;master=false", devices.get(0).getBluetoothAddress());
 
         TestBtClient client = new TestBtClient(url);
 
-        client.connect().addListener(new DebugChannelListener());
+        ChannelFutureListener listener = new ActionChannelFutureListener()
+                .whenSuccess((l, cf) -> {
+                    executor.scheduleAtFixedRate(() -> {
+                        byte[] msg = new byte[300];
+                        Arrays.fill(msg, (byte) 1);
+                        client.writeAndFlush(Unpooled.wrappedBuffer(msg));
+                    }, 2, 30, TimeUnit.MILLISECONDS);
+
+                    JscChannelConfig config = (JscChannelConfig) cf.channel().config();
+                    Console.log(config.getBaudRate());
+                })
+                .whenCancel((l, cf) -> Console.log("cancel"))
+                .whenDone((l, cf) -> Console.log("done"));
+
+
+        client.connect().addListener(listener);
 
     }
 
