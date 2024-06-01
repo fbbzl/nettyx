d700e8a809b75e6f77fb4f1deef02c99e8916849
fengbinbin <290361638@qq.com>
2024-06-01 10:12:57 +0800

    feat(upgrade): update 2.3.1 refactor package

---
+++ src/main/java/org/fz/nettyx/ssl/OpenSslManagement.java
@@ -0,0 +1,17 @@
+package org.fz.nettyx.ssl;
+
+/**
+ * TODO
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/6/1 10:11
+ */
+
+public class OpenSslManagement {
+
+    // 生成客户端证书
+
+
+    // 生成服务端证书
+
+}
