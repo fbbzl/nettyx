bd8fc7baf9b281a7e45fb8dc96801dd19d484050
fengbinbin <290361638@qq.com>
2024-06-02 21:53:38 +0800

    feat(upgrade): update 2.3.1 refactor package

--- src/main/java/org/fz/nettyx/ssl/OpenSslManagement.java
+++ src/main/java/org/fz/nettyx/ssl/OpenSslManagement.java
@@ -2,6 +2,7 @@ package org.fz.nettyx.ssl;
 
 /**
  * TODO
+ *
  * @author fengbinbin
  * @version 1.0
  * @since 2024/6/1 10:11
@@ -10,10 +11,16 @@ package org.fz.nettyx.ssl;
 public class OpenSslManagement {
 
     // 生成根证书
+    public static void createRootCertificate() {
+
+    }
 
     // 生成客户端证书
-
+    public static void createClientCertificate() {
+    }
 
     // 生成服务端证书
+    public static void createServerCertificate() {
 
+    }
 }
