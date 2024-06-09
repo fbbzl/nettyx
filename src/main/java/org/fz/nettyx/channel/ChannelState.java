16d16545560d55fbe3863b1b494fb8083e31add9
fengbinbin <290361638@qq.com>
2024-06-09 15:31:58 +0800

    feat(upgrade): update 2.3.1 refactor package

--- src/main/java/org/fz/nettyx/channel/ChannelState.java
+++ src/main/java/org/fz/nettyx/channel/ChannelState.java
@@ -2,6 +2,9 @@ package org.fz.nettyx.channel;
 
 /**
  * to save connect state history
+ *
+ * @author fengbinbin
+ * @since 2021 -12-29 18:46
  */
 public class ChannelState {
     private int connectTimes;
