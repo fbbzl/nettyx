c817cc5f8826ef60fe7e142bc6003a7fefacb41c
fengbinbin <290361638@qq.com>
2024-06-10 20:47:37 +0800

    feat(upgrade): update 2.3.1 refactor package

---
+++ src/main/java/org/fz/nettyx/exception/StopRedoException.java
@@ -0,0 +1,30 @@
+package org.fz.nettyx.exception;
+
+/**
+ * @author fengbinbin
+ * @version 1.0
+ * @since 2024/6/10 20:40
+ */
+public class StopRedoException extends RuntimeException {
+
+    public StopRedoException() {
+        super();
+    }
+
+    public StopRedoException(String message) {
+        super(message);
+    }
+
+    public StopRedoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StopRedoException(Throwable cause) {
+        super(cause);
+    }
+
+    protected StopRedoException(String message, Throwable cause, boolean enableSuppression,
+                                boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
