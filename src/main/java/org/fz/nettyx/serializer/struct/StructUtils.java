3827c218a25b1d1d5a683f9d9a7abfbdef7de69d
fengbinbin <290361638@qq.com>
2024-06-21 10:34:15 +0800

    feat(upgrade): update 2.3.2 refactor package

--- src/main/java/org/fz/nettyx/serializer/struct/StructUtils.java
+++ src/main/java/org/fz/nettyx/serializer/struct/StructUtils.java
@@ -177,13 +177,8 @@ public class StructUtils {
      */
     public static <S> S newStruct(Type structClass) {
         try {
-            if (structClass instanceof Class) {
-                return ReflectUtil.getConstructor((Class<S>) structClass).newInstance();
-            }
-
-            if (structClass instanceof ParameterizedType) {
-                return ReflectUtil.getConstructor((Class<S>) ((ParameterizedType) structClass).getRawType()).newInstance();
-            }
+            if (structClass instanceof Class)             return ReflectUtil.getConstructor((Class<S>) structClass).newInstance();
+            if (structClass instanceof ParameterizedType) return ReflectUtil.getConstructor((Class<S>) ((ParameterizedType) structClass).getRawType()).newInstance();
 
             throw new UnsupportedOperationException("can not create instance of type [" + structClass + "], can not find @Struct annotation on class");
         } catch (IllegalAccessException | InvocationTargetException | InstantiationException exception) {
