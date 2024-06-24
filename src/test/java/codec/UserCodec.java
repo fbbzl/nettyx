7c81e894718a3bbb2fa205563cbee477a37fe990
fengbinbin <290361638@qq.com>
2024-06-25 00:00:11 +0800

    feat(upgrade): update 2.3.2 and refactor package

--- src/test/java/codec/UserCodec.java
+++ src/test/java/codec/UserCodec.java
@@ -1,13 +1,14 @@
 package codec;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.StopWatch;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.lang.reflect.MethodHandleUtil;
-import cn.hutool.core.util.ReflectUtil;
 import codec.model.*;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.Data;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.fz.nettyx.serializer.struct.StructSerializer;
@@ -17,9 +18,11 @@ import org.fz.nettyx.serializer.struct.basic.c.unsigned.Cuchar;
 import org.fz.nettyx.serializer.struct.basic.c.unsigned.Culong8;
 
 import java.lang.invoke.MethodHandle;
-import java.lang.reflect.Constructor;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
+import static java.lang.invoke.MethodType.methodType;
 import static org.fz.nettyx.template.AbstractMultiChannelTemplate.channelKey;
 
 
@@ -41,32 +44,37 @@ public class UserCodec extends SimpleChannelInboundHandler<ByteBuf> {
         Console.print(s.prettyPrint(TimeUnit.MILLISECONDS));
     }
 
-    public static class X {
+    @Data
+    public static class X<A> {
         private int a;
-    }
 
-    static Constructor<X> userConstructor   = ReflectUtil.getConstructor(X.class);
-    static MethodHandle   constructorHandle = MethodHandleUtil.findConstructor(X.class);
+        public void setA(int a) {
+            this.a = a;
+        }
+    }
 
     @SneakyThrows
     public static void main(String[] args) {
+        X x = new X();
         StopWatch s = new
                 StopWatch("create");
-        s.start("new");
-        for (int i = 0; i < 100_000_000; i++) {
-            X x = new X();
+        s.start("set");
+        for (int i = 0; i < 100_000; i++) {
+            x.setA(1);
         }
         s.stop();
 
+        Method setter = BeanUtil.getPropertyDescriptor(X.class, "a").getWriteMethod();
         s.start("reflect");
-        for (int i = 0; i < 100_000_000; i++) {
-            X x = userConstructor.newInstance();
+        for (int i = 0; i < 100_000; i++) {
+            MethodHandleUtil.invoke(x, setter, 1);
         }
         s.stop();
 
         s.start("methodhandle");
-        for (int i = 0; i < 100_000_000; i++) {
-            X x = (X) constructorHandle.invoke();
+        MethodHandle methodhandle = MethodHandles.lookup().findVirtual(X.class, "setA", methodType(void.class, int.class));
+        for (int i = 0; i < 100_000; i++) {
+            methodhandle.invokeWithArguments(x,1);
         }
         s.stop();
 
