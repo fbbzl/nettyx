38f2c75f7bd6598698aad7105131fa6d104e22c3
fengbinbin <290361638@qq.com>
2024-07-05 22:18:34 +0800

    feat(upgrade): update 2.3.3 optimize reflect by method handle

--- src/main/java/org/fz/nettyx/serializer/struct/annotation/ToString.java
+++ src/main/java/org/fz/nettyx/serializer/struct/annotation/ToString.java
@@ -70,9 +70,5 @@ public @interface ToString {
             if (value != null) writing.writeBytes(value.toString().getBytes(Charset.forName(charset)));
             else               writing.writeBytes(new byte[bufferLength]);
         }
-
-        public static void toExceptionNotice() {// TODO
-
-        }
     }
 }
