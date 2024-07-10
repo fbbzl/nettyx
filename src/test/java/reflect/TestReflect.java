42a197d8c032f89d924ccef00431b41ffd339070
fengbinbin <290361638@qq.com>
2024-07-10 22:41:41 +0800

    feat(upgrade): update 2.3.3 optimize reflect by method handle

--- src/test/java/reflect/TestReflect.java
+++ src/test/java/reflect/TestReflect.java
@@ -46,24 +46,28 @@ public class TestReflect {
         stopWatch.start("new");
         for (int i = 0; i < 1_000_000_000; i++) {
             Order order = new Order();
+            int   i1    = order.hashCode();
         }
         stopWatch.stop();
 
         stopWatch.start("supplier");
         for (int i = 0; i < 1_000_000_000; i++) {
             Order order = supplier.get();
+            int   i1    = order.hashCode();
         }
         stopWatch.stop();
 
         stopWatch.start("ref construct");
         for (int i = 0; i < 1_000_000_000; i++) {
             Order order = refConstruct.newInstance();
+            int   i1    = order.hashCode();
         }
         stopWatch.stop();
 
         stopWatch.start("method handle");
         for (int i = 0; i < 1_000_000_000; i++) {
-            Object invoke = methodHandle.invoke();
+            Object order = methodHandle.invoke();
+            int   i1    = order.hashCode();
         }
         stopWatch.stop();
 
