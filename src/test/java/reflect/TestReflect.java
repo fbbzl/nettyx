d66eb63b0069f8d72908e3cc201f13e833b99724
fengbinbin <290361638@qq.com>
2024-07-29 15:38:44 +0800

    test(reflect): add reflectasm

--- src/test/java/reflect/TestReflect.java
+++ src/test/java/reflect/TestReflect.java
@@ -3,6 +3,7 @@ package reflect;
 import cn.hutool.core.date.StopWatch;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.ReflectUtil;
+import com.esotericsoftware.reflectasm.ConstructorAccess;
 import lombok.Data;
 import org.junit.Test;
 
@@ -19,34 +20,36 @@ import static java.lang.invoke.MethodType.methodType;
  * @version 1.0
  * @since 2024/7/8 22:25
  */
+
+@SuppressWarnings("all")
 public class TestReflect {
 
     @Data
     public static class Order {
-        private int id;
-        private String  username;
-        private int iffd;
-        private String  usefdfrname;
-        private int idfd;
-        private String  useasfdrname;
-        private int id1;
-        private String  username2;
-        private Integer i34d;
-        private Double   use34rname;
-        private Integer iff34d;
+        private int       id;
+        private String    username;
+        private int       iffd;
+        private String    usefdfrname;
+        private int       idfd;
+        private String    useasfdrname;
+        private int       id1;
+        private String    username2;
+        private Integer   i34d;
+        private Double    use34rname;
+        private Integer   iff34d;
         private StopWatch usefd34frname = new StopWatch();
-        private Integer idf34d;
-        private long  useas34fdrname;
-        private int id341;
-        private char  user34name2;
+        private Integer   idf34d;
+        private long      useas34fdrname;
+        private int       id341;
+        private char      user34name2;
     }
 
     private static final Constructor<Order>       refConstruct = ReflectUtil.getConstructor(Order.class);
     private static final MethodHandle             constructorHandle;
     private static final Supplier<Order>          supplier     = Order::new;
-    //private static final ConstructorAccess<Order> access       = ConstructorAccess.get(Order.class);
+    private static final ConstructorAccess<Order> access       = ConstructorAccess.get(Order.class);
 
-    private static final Supplier orderSupplier;
+    private static final Supplier<Order> orderSupplier;
 
     static {
         try {
@@ -61,7 +64,7 @@ public class TestReflect {
                     constructorHandle,
                     constructorHandle.type());
 
-            orderSupplier = (Supplier) site.getTarget().invokeExact();
+            orderSupplier = (Supplier<Order>) site.getTarget().invokeExact();
         } catch (Throwable e) {
             throw new RuntimeException(e);
         }
@@ -70,56 +73,53 @@ public class TestReflect {
 
     @Test
     public void testReflect() throws Throwable {
-        StopWatch stopWatch = StopWatch.create("");
 
-        int times = 10_000_000;
-        //
-//        for (int i = 0; i < times; i++) {
-//            Order order = new Order();
-//        }
 
+        Order order = new Order();
+        for (int round = 0; round < 10; round++) {
+            int       times     = 10000;
+            StopWatch stopWatch = StopWatch.create("");
 //        stopWatch.start("new");
 //        for (int i = 0; i < times; i++) {
 //            Order order = new Order();
 //        }
 //        stopWatch.stop();
-
+//
 //        stopWatch.start("supplier");
 //        for (int i = 0; i < times; i++) {
 //            Order order = supplier.get();
 //        }
 //        stopWatch.stop();
-
+//
 //        stopWatch.start("reflect construct");
 //        for (int i = 0; i < times; i++) {
 //            Order order = refConstruct.newInstance();
 //        }
 //        stopWatch.stop();
-
+//
 //        stopWatch.start("method handle");
 //        for (int i = 0; i < times; i++) {
 //            Object invoke = constructorHandle.invoke();
 //        }
 //        stopWatch.stop();
 
-        ////////////////////////
 
-        stopWatch.start("lambda meta factory");
-        for (int i = 0; i < times; i++) {
-            Order refAsm = (Order) orderSupplier.get();
+            stopWatch.start("lambda meta factory");
+            for (int i = 0; i < times; i++) {
+                Order refAsm = (Order) orderSupplier.get();
+            }
+            stopWatch.stop();
+
+
+            stopWatch.start("reflectAsm");
+            for (int i = 0; i < times; i++) {
+                Order refAsm = access.newInstance();
+            }
+            stopWatch.stop();
+
+            Console.log(stopWatch.prettyPrint(TimeUnit.NANOSECONDS));
         }
-        stopWatch.stop();
 
-//        stopWatch.start("reflectAsm");
-//        for (int i = 0; i < times; i++) {
-//            Order refAsm = access.newInstance();
-//        }
-//        stopWatch.stop();
-
-
-
-
-        Console.log(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
     }
 
 }
