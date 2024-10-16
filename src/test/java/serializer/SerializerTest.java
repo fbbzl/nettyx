14403f40ff5b68ca43432956679552fea7073c25
fengbinbin <290361638@qq.com>
2024-10-16 12:44:42 +0800

    feat(update): update 2.3.16

--- src/test/java/serializer/SerializerTest.java
+++ src/test/java/serializer/SerializerTest.java
@@ -1,15 +1,11 @@
 package serializer;
 
-import cn.hutool.core.lang.Console;
 import codec.model.*;
 import org.fz.nettyx.serializer.struct.StructSerializer;
 import org.fz.nettyx.serializer.struct.TypeRefer;
 import org.fz.nettyx.serializer.struct.basic.c.signed.Clong4;
 import org.fz.nettyx.serializer.struct.basic.c.signed.Clong8;
-import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Arrays;
 
 /**
  * @author fengbinbin
@@ -23,12 +19,22 @@ public class SerializerTest {
 
     @Test
     public void testStructSerializer() {
-        byte[] bytes = new byte[1024 * 6];
-        Arrays.fill(bytes, (byte) 67);
+        // byte[] bytes = new byte[1024 * 6];
+        // Arrays.fill(bytes, (byte) 67);
 
         User user = new User<>();
 
-        Console.log("read :" + user);
+		//setNullForTest(user);
+
+        final byte[] userWriteBytes = StructSerializer.toBytes(userTypeRefer, user);
+        System.err.println("userWriteBytes: " + userWriteBytes.length);
+        User turn = StructSerializer.toStruct(userTypeRefer, userWriteBytes);
+
+        System.err.println(turn.equals(user));
+    }
+
+
+    public void setNullForTest(User user) {
         user.setAddress(null);
         user.setQqNames(null);
         user.setSs(null);
@@ -42,19 +48,5 @@ public class SerializerTest {
         user.setWwife(null);
         user.setWives(null);
         user.setWives121212(null);
-
-
-        final byte[] userWriteBytes = StructSerializer.toBytes(userTypeRefer, user);
-        System.err.println("userWriteBytes: " + userWriteBytes.length);
-        User turn = StructSerializer.toStruct(userTypeRefer, userWriteBytes);
-
-        System.err.println(turn.equals(user));
-        byte[] bytes1 = StructSerializer.toBytes(userTypeRefer, new User<>());
-        System.err.println("bytes1: " + bytes1.length);
-        Console.log(Arrays.toString(bytes1));
-        Console.log("turn :" + turn);
-
-        Assert.assertNotNull(turn);
     }
-
 }
