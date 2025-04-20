bcf961fd73172db47bf7b040716a93c1bb605361
fengbinbin <29036163@qq.com>
2025-04-20 11:41:39 +0800

    feat(update): update 2.3.27

--- src/main/java/org/fz/nettyx/serializer/struct/annotation/ToLinkedList.java
+++ src/main/java/org/fz/nettyx/serializer/struct/annotation/ToLinkedList.java
@@ -4,7 +4,7 @@ import io.netty.buffer.ByteBuf;
 import org.fz.nettyx.exception.ParameterizedTypeException;
 import org.fz.nettyx.serializer.struct.StructFieldHandler;
 import org.fz.nettyx.serializer.struct.StructSerializer;
-import org.fz.nettyx.util.Throws;
+import org.fz.util.exception.Throws;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -53,7 +53,7 @@ public @interface ToLinkedList {
         public Object doRead(StructSerializer serializer, Type fieldType, Field field, ToLinkedList toLinkedList) {
             Type elementType = serializer.getElementType(fieldType);
 
-            Throws.ifTrue(elementType == Object.class, new ParameterizedTypeException(field));
+            Throws.ifTrue(elementType == Object.class, () -> new ParameterizedTypeException(field));
 
             return serializer.readList(elementType, toLinkedList.size(), new LinkedList<>());
         }
@@ -62,7 +62,7 @@ public @interface ToLinkedList {
         public void doWrite(StructSerializer serializer, Type fieldType, Field field, Object value, ToLinkedList toLinkedList, ByteBuf writing) {
             Type elementType = serializer.getElementType(fieldType);
 
-            Throws.ifTrue(elementType == Object.class, new ParameterizedTypeException(field));
+            Throws.ifTrue(elementType == Object.class, () -> new ParameterizedTypeException(field));
 
             serializer.writeList(defaultIfNull((List<?>) value, Collections::emptyList), elementType, toLinkedList.size(), writing);
         }
