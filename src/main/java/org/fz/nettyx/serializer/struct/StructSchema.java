ade5e6e88057b0246fb08b812301779ba6870f47
fengbinbin <29036163@qq.com>
2025-04-28 20:32:22 +0800

    feat(update): update 2.4.2

--- src/main/java/org/fz/nettyx/serializer/struct/StructSchema.java
+++ src/main/java/org/fz/nettyx/serializer/struct/StructSchema.java
@@ -1,21 +1,17 @@
 package org.fz.nettyx.serializer.struct;
 
-import cn.hutool.core.util.TypeUtil;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.fz.nettyx.serializer.struct.StructDefinition.StructField;
 import org.fz.nettyx.util.TypeRefer;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
 
-import static org.fz.nettyx.serializer.struct.StructHelper.getRawType;
+import static org.fz.nettyx.serializer.struct.StructFieldHandler.DEFAULT_STRUCT_FIELD_HANDLER;
 import static org.fz.nettyx.serializer.struct.StructSerializerContext.getStructDefinition;
 
 /**
@@ -27,27 +23,40 @@ import static org.fz.nettyx.serializer.struct.StructSerializerContext.getStructD
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class StructSchema {
 
-    final Map<StructField, Type> classMap;
+    final Map<StructField, Type> schema;
 
     public static <T> StructSchema of(TypeRefer<T> type) {
+        Type         root   = type.getTypeValue();
         StructSchema schema = new StructSchema(new HashMap<>(16));
 
-        StructDefinition structDef = getStructDefinition(getRawType(type));
+        StructDefinition structDef = getStructDefinition(type);
 
-        for (StructField structField : structDef.fields()) {
-            Field                      field           = null;
-            Function<Object, Object>   getter          = structField.getter();
-            BiConsumer<Object, Object> setter          = structField.setter();
-            Annotation                 annotation      = structField.annotation();
-            StructFieldHandler<?>      handler         = structField.handler();
-            Type                       fieldActualType = TypeUtil.getActualType(type, field);
-
-            schema.getClassMap().put(null, fieldActualType);
-        }
         return schema;
     }
 
+    protected void getSchema(Type root, StructField structField, Map<StructField, Type> cumulative) {
+        Field wrapped = structField.wrapped();
 
+        if (DEFAULT_STRUCT_FIELD_HANDLER.isBasic(root, wrapped)) {
+            cumulative.put(structField, structField.type(root));
+        }
+        if (DEFAULT_STRUCT_FIELD_HANDLER.isStruct(root, wrapped)) {
+            StructDefinition structDef = getStructDefinition(structField.type(root));
+            if (structDef == null) return;
 
+            for (StructField sf : structDef.fields()) {
+                this.warmup(sf);
+                this.getSchema(root, sf, cumulative);
+            }
+        }
+    }
+
+    private void warmup(StructField sf) {
+        sf.wrapped();
+        sf.getter();
+        sf.setter();
+        sf.annotation();
+        sf.handler();
+    }
 
 }
