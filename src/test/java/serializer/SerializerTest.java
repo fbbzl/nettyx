3fa05167153cb32c8d8e7479cfea0c9ddd70e39a
fengbinbin <29036163@qq.com>
2025-05-08 19:43:22 +0800

    feat(update): update 2.4.4

--- src/test/java/serializer/SerializerTest.java
+++ src/test/java/serializer/SerializerTest.java
@@ -33,15 +33,14 @@ public class SerializerTest {
 
     @Test
     public void testStructSerializer() {
-        byte[] bytes = new byte[6411];
+        byte[] bytes = new byte[800];
         Arrays.fill(bytes, (byte) 67);
 
         StopWatch stopWatch  = StopWatch.create("反序列");
         stopWatch.start();
-        for (int i = 0; i < 111; i++) {
-            Object user = StructSerializer.toStruct(userTypeRefer, bytes);
-            byte[] byteBuf = StructSerializer.toBytes(userTypeRefer, user);
-            byte[] byteBufa = StructSerializer.toBytes(userTypeRefer, new User<>());
+        for (int i = 0; i < 1_000_000; i++) {
+             StructSerializer.toStruct(userTypeRefer, bytes);
+
         }
         stopWatch.stop();
         Console.print(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
