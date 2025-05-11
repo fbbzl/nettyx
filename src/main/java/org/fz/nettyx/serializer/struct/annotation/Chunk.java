0eaea3f9dd6e74a2d955c83d071bb3319d8bac61
fengbinbin <290361638@qq.com>
2025-05-11 13:15:53 +0800

    refactor(serializer): 2.5.3 chunk

--- src/main/java/org/fz/nettyx/serializer/struct/annotation/Chunk.java
+++ src/main/java/org/fz/nettyx/serializer/struct/annotation/Chunk.java
@@ -3,19 +3,23 @@ package org.fz.nettyx.serializer.struct.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
+ * chunk only support for [byte[]/ByteBuf] type field
  * @author fengbinbin
  * @version 1.0
  * @since 2025/5/11 13:05
  */
 
-@Target(TYPE)
+@Target(FIELD)
 @Retention(RUNTIME)
 public @interface Chunk {
 
+    /**
+     * the number of bytes that need to be occupied
+     */
     int size();
 
 }
