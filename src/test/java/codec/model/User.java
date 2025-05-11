11bf214638248479b0269f5ac1d4a1449132a13b
fengbinbin <290361638@qq.com>
2025-05-11 11:00:39 +0800

    refactor(serializer): 2.5.3

--- src/test/java/codec/model/User.java
+++ src/test/java/codec/model/User.java
@@ -3,10 +3,7 @@ package codec.model;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
-import org.fz.nettyx.serializer.struct.annotation.Ignore;
-import org.fz.nettyx.serializer.struct.annotation.Struct;
-import org.fz.nettyx.serializer.struct.annotation.ToArray;
-import org.fz.nettyx.serializer.struct.annotation.ToArrayList;
+import org.fz.nettyx.serializer.struct.annotation.*;
 import org.fz.nettyx.serializer.struct.basic.c.signed.clong4;
 import org.fz.nettyx.serializer.struct.basic.c.stdint.unsigned.cuint8_t;
 import org.fz.nettyx.serializer.struct.basic.c.unsigned.cuchar;
@@ -20,6 +17,8 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User<T, W, G> {
 
+    @ToCharSequence(bufferLength = 2)
+    String xname;
     clong4 uid;
     @ToArray(length = 5)
     cppushort[]     qqNames;
