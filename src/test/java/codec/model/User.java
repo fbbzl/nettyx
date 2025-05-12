9150f3ffdd93dc6bd18823022090e1ddd351e034
fengbinbin <29036163@qq.com>
2025-05-12 16:15:29 +0800

    test changes

--- src/test/java/codec/model/User.java
+++ src/test/java/codec/model/User.java
@@ -3,50 +3,42 @@ package codec.model;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
-import org.fz.nettyx.serializer.struct.annotation.*;
-import org.fz.nettyx.serializer.struct.basic.c.signed.clong4;
-import org.fz.nettyx.serializer.struct.basic.c.stdint.unsigned.cuint8_t;
-import org.fz.nettyx.serializer.struct.basic.c.unsigned.cuchar;
-import org.fz.nettyx.serializer.struct.basic.c.unsigned.culong8;
-import org.fz.nettyx.serializer.struct.basic.cpp.unsigned.cppushort;
-
-import java.util.List;
+import org.fz.nettyx.serializer.struct.annotation.Struct;
 
 @Data
 @Struct
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User<T, W, G> {
 
-    @ToCharSequence(bufferLength = 2)
-    String xname;
-    clong4 uid;
-    @ToArray(length = 5)
-    cppushort[]     qqNames;
-    @ToArrayList(size = 5)
-    List<cppushort> ss;
-    @ToArrayList(size = 5)
-    List<cppushort> tts;
+    //    @ToCharSequence(bufferLength = 2)
+//    String xname;
+//    clong4 uid;
+//    @ToArray(length = 5)
+//    cppushort[]     qqNames;
+//    @ToArray(length = 5)
+//    List<cppushort> ss;
+//    @ToArray(length = 5)
+//    List<cppushort> tts;
+//
+//    Bom<T, W, G> b;
+//    @ToArray(length = 5)
+//    Bom<T, W, clong4>[]                         g111fs;
+    Bom<T, Son<W, Bill>, GirlFriend>[] bs2d;
 
-    Bom<T, W, G> b;
-    @ToArray(length = 5)
-    Bom<T, W, clong4>[]                         g111fs;
-    @ToArrayList(size = 5)
-    List<Bom<T, Son<cuchar, Bill>, GirlFriend>> bs2d;
-
-    T sonsbaba;
-    @ToArray(length = 5)
-    T[]     sonff;
-    @ToArrayList(size = 5)
-    List<T> so111ns;
-
-    W wwife;
-    @ToArrayList(size = 5)
-    List<W> wives;
-    @ToArray(length = 5)
-    W[]     wives121212;
-
-    transient cuint8_t uname;
-
-    @Ignore
-    culong8 deviceS;
+//    T sonsbaba;
+//    @ToArray(length = 5)
+//    T[]     sonff;
+//    @ToArray(length = 5)
+//    List<T> so111ns;
+//
+//    W wwife;
+//    @ToArray(length = 5)
+//    List<W> wives;
+//    @ToArray(length = 5)
+//    W[]     wives121212;
+//
+//    transient cuint8_t uname;
+//
+//    @Ignore
+//    culong8 deviceS;
 }
