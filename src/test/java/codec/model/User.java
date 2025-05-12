84fa7fc333b5e6c4348ea3f9fe07c0fe45622bb3
fengbinbin <29036163@qq.com>
2025-05-12 22:47:40 +0800

    feat(update): update 2.5.4-rollback mode

--- src/test/java/codec/model/User.java
+++ src/test/java/codec/model/User.java
@@ -3,44 +3,41 @@ package codec.model;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
+import org.fz.nettyx.serializer.struct.annotation.Ignore;
 import org.fz.nettyx.serializer.struct.annotation.Struct;
 import org.fz.nettyx.serializer.struct.annotation.ToArray;
+import org.fz.nettyx.serializer.struct.annotation.ToCharSequence;
+import org.fz.nettyx.serializer.struct.basic.c.signed.clong4;
+import org.fz.nettyx.serializer.struct.basic.c.stdint.unsigned.cuint8_t;
+import org.fz.nettyx.serializer.struct.basic.c.unsigned.culong8;
+import org.fz.nettyx.serializer.struct.basic.cpp.unsigned.cppushort;
 
 @Data
 @Struct
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User<T, W, G> {
 
-    //    @ToCharSequence(bufferLength = 2)
-//    String xname;
-//    clong4 uid;
-//    @ToArray(length = 5)
-//    cppushort[]     qqNames;
-//    @ToArray(length = 5)
-//    List<cppushort> ss;
-//    @ToArray(length = 5)
-//    List<cppushort> tts;
-//
-//    Bom<T, W, G> b;
-//    @ToArray(length = 5)
-//    Bom<T, W, clong4>[]                         g111fs;
-    @ToArray(length = 2)
-    Bom<T, Son<W, Bill>, GirlFriend>[] bs2d;
+    @ToCharSequence(bufferLength = 2)
+    String xname;
+    clong4 uid;
 
-//    T sonsbaba;
-//    @ToArray(length = 5)
-//    T[]     sonff;
-//    @ToArray(length = 5)
-//    List<T> so111ns;
-//
-//    W wwife;
-//    @ToArray(length = 5)
-//    List<W> wives;
-//    @ToArray(length = 5)
-//    W[]     wives121212;
-//
-//    transient cuint8_t uname;
-//
-//    @Ignore
-//    culong8 deviceS;
+    @ToArray(length = 5)
+    cppushort[] tts;
+
+    Bom<T, W, G> b;
+    @ToArray(length = 5)
+    Bom<T, W, clong4>[] g111fs;
+
+    T sonsbaba;
+    @ToArray(length = 5)
+    T[] sonff;
+
+    W wwife;
+    @ToArray(length = 5)
+    W[] wives121212;
+
+    transient cuint8_t uname;
+
+    @Ignore
+    culong8 deviceS;
 }
