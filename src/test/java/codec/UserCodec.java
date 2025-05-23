fc931fea05c6aa45d811ba039750881f81c336db
fengbinbin <290361638@qq.com>
2025-05-23 09:36:30 +0800

    refactor(serializer): 2.5.8

--- src/test/java/codec/UserCodec.java
+++ src/test/java/codec/UserCodec.java
@@ -31,7 +31,7 @@ public class UserCodec extends StructCodec<User<clong4, Wife<culong8, Son<clong4
         stopWatch.start("decode");
         super.decode(ctx, msg, out);
         stopWatch.stop();
-        Console.log(stopWatch.prettyPrint(TimeUnit.NANOSECONDS));
+        Console.log(stopWatch.prettyPrint(TimeUnit.MICROSECONDS));
     }
 
     @Override
@@ -43,6 +43,6 @@ public class UserCodec extends StructCodec<User<clong4, Wife<culong8, Son<clong4
 
         super.encode(ctx, struct, out);
         stopWatch.stop();
-        Console.log(stopWatch.prettyPrint(TimeUnit.NANOSECONDS));
+        Console.log(stopWatch.prettyPrint(TimeUnit.MICROSECONDS));
     }
 }
