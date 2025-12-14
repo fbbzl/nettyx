2ea405a8f1db98b7a24b4cba2ac0ec4de05f2ba8
fengbinbin <290361638@qq.com>
2025-12-14 10:27:26 +0800

    refactor(struct): Simplifies the byte-ordered enum names of the structure
    - Rename Struct.Endian.BIG to BE
    - Rename Struct.Endian.LITTLE to LE
    - Updated references to byte order enums in test classes

--- src/test/java/codec/model/Brother.java
+++ src/test/java/codec/model/Brother.java
@@ -2,23 +2,25 @@ package codec.model;
 
 import lombok.Data;
 import org.fz.nettyx.serializer.struct.annotation.Struct;
+import org.fz.nettyx.serializer.struct.annotation.Struct.Endian;
 import org.fz.nettyx.serializer.struct.annotation.ToArray;
+import org.fz.nettyx.serializer.struct.basic.c.signed.cint;
 
 /**
  * @author fengbinbin
  * @version 1.0
  * @since 2025/9/26 0:41
  */
-@Struct
+@Struct(endian = Endian.BE)
 @Data
 public class Brother {
 //    @ToArray(flexible = true)
 //    cint[] ages;
 
-//    @ToArray(flexible = true)
-//    Son<cint, cint>[] sons;
-
     @ToArray(flexible = true)
-    Lover[] lovers;
+    Son<cint, cint>[] sons;
+
+//    @ToArray(flexible = true)
+//    Lover[] lovers;
 
 }
